57402223f8b68f2c6deb47473e9e9e35ada1ba31
jacobi petrucciani <jacobi@hackerrank.com>
2020-11-23 14:43:43 -0500

    updates to the git block

--- home.nix
+++ home.nix
@@ -39,6 +39,7 @@ in {
     gnugrep
     gnused
     gnutar
+    gron
     gzip
     htop
     less
@@ -116,93 +117,102 @@ in {
     '')
   ];
 
-  home.file.sqliterc = {
-    target = ".sqliterc";
-    text = ''
-      .output /dev/null
-      .headers on
-      .mode column
-      .prompt "> " ". "
-      .separator ROW "\n"
-      .nullvalue NULL
-      .output stdout
-    '';
+  home.file = {
+    sqliterc = {
+      target = ".sqliterc";
+      text = ''
+        .output /dev/null
+        .headers on
+        .mode column
+        .prompt "> " ". "
+        .separator ROW "\n"
+        .nullvalue NULL
+        .output stdout
+      '';
+    };
   };
 
   # starship config
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    add_newline = false;
-    character = {
-      symbol = promptChar;
-      success_symbol = "[${promptChar}](bright-green)";
-      error_symbol = "[${promptChar}](bright-red)";
-    };
-    golang = {
-      style = "fg:#00ADD8";
-      symbol = "go ";
-    };
-    directory.style = "fg:#d442f5";
-    nix_shell = {
-      pure_msg = "";
-      impure_msg = "";
-      format = "via [$symbol$state]($style) ";
-    };
-    kubernetes = {
-      disabled = false;
-      style = "fg:#326ce5";
-    };
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        symbol = promptChar;
+        success_symbol = "[${promptChar}](bright-green)";
+        error_symbol = "[${promptChar}](bright-red)";
+      };
+      golang = {
+        style = "fg:#00ADD8";
+        symbol = "go ";
+      };
+      directory.style = "fg:#d442f5";
+      nix_shell = {
+        pure_msg = "";
+        impure_msg = "";
+        format = "via [$symbol$state]($style) ";
+      };
+      kubernetes = {
+        disabled = false;
+        style = "fg:#326ce5";
+      };
 
-    # disabled plugins
-    aws.disabled = true;
-    cmd_duration.disabled = true;
-    gcloud.disabled = true;
-    package.disabled = true;
+      # disabled plugins
+      aws.disabled = true;
+      cmd_duration.disabled = true;
+      gcloud.disabled = true;
+      package.disabled = true;
+    };
   };
 
   # gitconfig
-  programs.git.enable = true;
-  programs.git.userName = "${firstName} ${lastName}";
-  programs.git.userEmail = if isDarwin then workEmail else personalEmail;
-  programs.git.aliases = {
-    A = "add -A";
-    pu = "pull";
-    pur = "pull --rebase";
-    cam = "commit -am";
-    ca = "commit -a";
-    cm = "commit -m";
-    ci = "commit";
-    co = "checkout";
-    st = "status";
-    br = "branch -v";
-    branch-name = "!git rev-parse --abbrev-ref HEAD";
-    # Delete the remote version of the current branch
-    unpublish = "!git push origin :$(git branch-name)";
-    # Push current branch
-    put = "!git push origin $(git branch-name)";
-    # Pull without merging
-    get = "!git pull origin $(git branch-name) --ff-only";
-    # Pull Master without switching branches
-    got =
-      "!f() { CURRENT_BRANCH=$(git branch-name) && git checkout $1 && git pull origin $1 --ff-only && git checkout $CURRENT_BRANCH;  }; f";
-    # Recreate your local branch based on the remote branch
-    recreate = ''
-      !f() { [[ -n $@ ]] && git checkout master && git branch -D "$@" && git pull origin "$@":"$@" && git checkout "$@"; }; f'';
-    reset-submodule = "!git submodule update --init";
-    sl = "!git --no-pager log -n 15 --oneline --decorate";
-    sla = "log --oneline --decorate --graph --all";
-    lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
-    lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-  };
-  programs.git.extraConfig = {
-    color.ui = true;
-    push.default = "simple";
-    pull.ff = "only";
-    core = {
-      editor = if isDarwin then "code --wait" else "nano";
-      pager = "delta --dark";
+  programs.git = {
+    enable = true;
+    userName = "${firstName} ${lastName}";
+    userEmail = if isDarwin then workEmail else personalEmail;
+    aliases = {
+      A = "add -A";
+      pu = "pull";
+      pur = "pull --rebase";
+      cam = "commit -am";
+      ca = "commit -a";
+      cm = "commit -m";
+      ci = "commit";
+      co = "checkout";
+      st = "status";
+      br = "branch -v";
+      hide = "update-index --skip-worktree";
+      unhide = "update-index --no-skip-worktree";
+      hidden = "! git ls-files -v | grep '^S' | cut -c3-";
+      branch-name = "!git rev-parse --abbrev-ref HEAD";
+      # Delete the remote version of the current branch
+      unpublish = "!git push origin :$(git branch-name)";
+      # Push current branch
+      put = "!git push origin $(git branch-name)";
+      # Pull without merging
+      get = "!git pull origin $(git branch-name) --ff-only";
+      # Pull Master without switching branches
+      got =
+        "!f() { CURRENT_BRANCH=$(git branch-name) && git checkout $1 && git pull origin $1 --ff-only && git checkout $CURRENT_BRANCH;  }; f";
+      # Recreate your local branch based on the remote branch
+      recreate = ''
+        !f() { [[ -n $@ ]] && git checkout master && git branch -D "$@" && git pull origin "$@":"$@" && git checkout "$@"; }; f'';
+      reset-submodule = "!git submodule update --init";
+      sl = "!git --no-pager log -n 15 --oneline --decorate";
+      sla = "log --oneline --decorate --graph --all";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
     };
-    rebase.instructionFormat = "<%ae >%s";
+    extraConfig = {
+      color.ui = true;
+      push.default = "simple";
+      pull.ff = "only";
+      core = {
+        editor = if isDarwin then "code --wait" else "nano";
+        pager = "delta --dark";
+      };
+      rebase.instructionFormat = "<%ae >%s";
+    };
   };
   programs.git.${if isDarwin then "signing" else null} = {
     key = "03C0CBEA6EAB9258";
