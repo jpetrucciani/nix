25ea3479277c3ca70a07f83829ca307e83829cbe
jacobi petrucciani <jacobi@hackerrank.com>
2020-12-08 19:05:26 -0500

    adding tmux and .tmux.conf

--- home.nix
+++ home.nix
@@ -351,4 +351,87 @@ in with pkgs.hax; {
     gpgPath = "gpg";
     signByDefault = true;
   };
+
+  programs.tmux = {
+    enable = true;
+    tmuxp.enable = true;
+    historyLimit = 500000;
+    extraConfig = ''
+      set -g base-index 1
+      set -g pane-base-index 1
+      # Setting the prefix from `C-b` to `C-a`.
+      # By remapping the `CapsLock` key to `Ctrl`,
+      # you can make triggering commands more comfottable!
+      set -g prefix C-a
+
+      set -g status-keys vi
+      setw -g mode-keys vi
+      setw -g mouse on
+      setw -g monitor-activity on
+
+      # Moving between windows.
+      unbind [
+      unbind ]
+      bind -r [ select-window -t :-
+      bind -r ] select-window -t :+
+
+      # Pane resizing.
+      bind -r H resize-pane -L 5
+      bind -r J resize-pane -D 5
+      bind -r K resize-pane -U 5
+      bind -r L resize-pane -R 5
+
+      # Maximize and restore a pane.
+      unbind Up
+      bind Up new-window -d -n tmp \; swap-pane -s tmp.1 \; select-window -t tmp
+      unbind Down
+      bind Down last-window \; swap-pane -s tmp.1 \; kill-window -t tmp
+
+      # Log output to a text file on demand.
+      bind P pipe-pane -o "cat >>~/#W.log" \; display "Toggled logging to ~/#W.log"
+
+
+      # -- display -------------------------------------------------------------------
+      # tabs
+      set -g window-status-current-format "#[fg=black]#[bg=red] #I #[bg=brightblack]#[fg=brightwhite] #W#[fg=brightblack]#[bg=black]"
+      set -g window-status-format "#[fg=black]#[bg=yellow] #I #[bg=brightblack]#[fg=brightwhite] #W#[fg=brightblack]#[bg=black]"
+
+      # status bar
+      set-option -g status-position bottom
+      set-option -g status-justify left
+      set -g status-fg colour1
+      set -g status-bg colour0
+      set -g status-left ' '
+      set -g status-right '#(date +"%_I:%M")'
+      set-option -g set-titles on
+      #256 colors
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",xterm-256color:Tc"
+      #Don't auto remane windows
+      set-option -g allow-rename off
+      # Source config
+      unbind r
+      bind r source-file ~/.tmux.conf \; display "Finished sourcing ~/.tmux.conf ."
+
+
+      # Use Alt-arrow keys without prefix key to switch panes
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
+
+      # Shift arrow to switch windows
+      bind -n S-Left  previous-window
+      bind -n S-Right next-window
+
+
+      # allow fn+left/right
+      bind-key -n Home send Escape "OH"
+      bind-key -n End send Escape "OF"
+
+      setw -g monitor-activity off
+      setw -g monitor-activity on
+      set-option -g bell-action none
+    '';
+  };
 }
