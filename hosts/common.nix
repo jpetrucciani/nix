5c44f8bbab2571f7c69eb8d94876abfba055b45c
jacobi petrucciani <j@cobi.dev>
2022-01-05 21:50:17 -0500

    hardening ssh

--- hosts/common.nix
+++ hosts/common.nix
@@ -134,6 +134,23 @@ in
       passwordAuthentication = false;
       permitRootLogin = "no";
       forwardX11 = true;
+      kexAlgorithms = [
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
+      ];
+      ciphers = [
+        "chacha20-poly1305@openssh.com"
+        "aes256-gcm@openssh.com"
+        # "aes128-gcm@openssh.com"
+        "aes256-ctr"
+        # "aes192-ctr"
+        # "aes128-ctr"
+      ];
+      macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+      ];
     };
   };
 
