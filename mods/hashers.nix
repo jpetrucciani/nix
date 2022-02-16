f9d83eff8c275eedfad75596809d17e520f0f4df
jacobi petrucciani <j@cobi.dev>
2022-02-16 09:12:00 -0500

    update hashers to have options and use the new writeBashBinCheckedWithFlags

--- mods/hashers.nix
+++ mods/hashers.nix
@@ -1,13 +1,24 @@
 prev: next: {
   _nix_hash = with next; with hax; repo: branch: name: (
-    writeBashBinChecked "nix_hash_${name}" ''
-      ${nix-prefetch-git}/bin/nix-prefetch-git \
-        --quiet \
-        --no-deepClone \
-        --branch-name ${branch} \
-        https://github.com/${repo}.git | \
-      ${jq}/bin/jq '{ date: (now|strflocaltime("%Y-%m-%d")), rev: .rev, sha256: .sha256 }'
-    ''
+    prev.writeBashBinCheckedWithFlags {
+      name = "nix_hash_${name}";
+      description = "grab the latest rev/sha256 from the '${branch}' branch of '${repo}'";
+      flags = [
+        {
+          name = "branch";
+          default = branch;
+          description = "set the branch to pull data from. default '${branch}'";
+        }
+      ];
+      script = ''
+        ${nix-prefetch-git}/bin/nix-prefetch-git \
+          --quiet \
+          --no-deepClone \
+          --branch-name "$branch" \
+          https://github.com/${repo}.git | \
+        ${jq}/bin/jq '{ date: (now|strflocaltime("%Y-%m-%d")), rev: .rev, sha256: .sha256 }'
+      '';
+    }
   );
   nix_hash_nixos_unstable = prev._nix_hash "NixOS/nixpkgs" "nixos-unstable" "nixos_unstable";
   nix_hash_unstable = prev._nix_hash "NixOS/nixpkgs" "nixpkgs-unstable" "unstable";
