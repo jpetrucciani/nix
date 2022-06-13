fbafc9fdff17e4ef78bc87626b4484938a597d1e
jacobi petrucciani <jacobi.petrucciani@medable.com>
2022-06-13 13:03:58 -0400

    revert attempt

--- mods/pkgs.nix
+++ mods/pkgs.nix
@@ -250,12 +250,4 @@ rec {
     sha256 = "sha256-3lc7eGtm7izLmnmiN7DpHTwnokchR0+VadWHjo651Po=";
   };
 
-  # temp fix for m1 macs
-  awscli2 =
-    if isM1 then (import
-      (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/d1c3fea7ecbed758168787fe4e4a3157e52bc808.tar.gz";
-        sha256 = "0ykm15a690v8lcqf2j899za3j6hak1rm3xixdxsx33nz7n3swsyy";
-      })
-      { }).awscli2 else prev.awscli2;
 }
