20d64a7f8c9ba6a9dc966de9c5fcbb9d4a7cce67
jacobi petrucciani <j@cobi.dev>
2022-07-11 20:29:58 -0400

    add lots of caddy plugins, try out new fork of geolocation

--- mods/caddy.nix
+++ mods/caddy.nix
@@ -4,17 +4,43 @@ rec {
   _xcaddy =
     let
       builtins = {
+        caddy-cgi = { name = "github.com/aksdb/caddy-cgi"; version = "7cf2523251ffeef310868d8ed03e17a929236f2e"; };
+        caddy-exec = { name = "github.com/abiosoft/caddy-exec"; version = "06d4f7218eb886ab9664e63c3f56010992e93fb9"; };
         caddy-security = { name = "github.com/greenpau/caddy-security"; version = "v1.1.14"; };
+        caddy-webhook = { name = "github.com/WingLim/caddy-webhook"; version = "v1.0.8"; };
+        certmagic-storage-dynamodb = { name = "github.com/silinternational/certmagic-storage-dynamodb"; version = "3.0.0"; };
+
+        # dns
+        alidns = { name = "github.com/caddy-dns/alidns"; version = "1.0.21"; };
+        azure = { name = "github.com/caddy-dns/azure"; version = "v0.2.0"; };
+        cloudflare = { name = "github.com/caddy-dns/cloudflare"; version = "91cf700356a1cd0127bcc4e784dd50ed85794af5"; };
+        digitalocean = { name = "github.com/caddy-dns/digitalocean"; version = "9c71e343246b954976c9294a7062823605de9b9f"; };
+        dnspod = { name = "github.com/caddy-dns/dnspod"; version = "v0.0.4"; };
+        duckdns = { name = "github.com/caddy-dns/duckdns"; version = "v0.3.1"; };
+        gandi = { name = "github.com/caddy-dns/gandi"; version = "v1.0.2"; };
+        hetzner = { name = "github.com/caddy-dns/hetzner"; version = "v0.0.1"; };
+        route53 = { name = "github.com/caddy-dns/route53"; version = "v1.2.0"; };
+        vultr = { name = "github.com/caddy-dns/vultr"; version = "733392841379526fd314012909963c3c6406687a"; };
+
+        # s3 stuff
         s3-proxy = { name = "github.com/lindenlab/caddy-s3-proxy"; version = "v0.5.6"; };
         s3-browser = { name = "github.com/techknowlogick/caddy-s3browser"; version = "81830de0e8d78d414488f1e621082ee732e7c3de"; };
-        geolocation = { name = "github.com/porech/caddy-maxmind-geolocation"; version = "89d86498ab7d55c9212c0c6b4d1ac9026929147b"; };
+
+        # utils
+        caddy-git = { name = "github.com/greenpau/caddy-git"; version = "v1.0.7"; };
+        caddy-json-parse = { name = "github.com/abiosoft/caddy-json-parse"; version = "c57039f26567f4b4120e35b4dc1a9bbd20a4f37f"; };
+        caddy-ratelimit = { name = "github.com/mholt/caddy-ratelimit"; version = "9c011f665e5ddff32fe00cab338ace7f360114ff"; };
+        caddy-trace = { name = "github.com/greenpau/caddy-trace"; version = "v1.1.10"; };
+        geolocation = { name = "github.com/jpetrucciani/caddy-maxmind-geolocation"; version = "aacfeb55604dba2d2fdd73570e023b1798092447"; };
+        replace_response = { name = "github.com/caddyserver/replace-response"; version = "d32dc3ffff0c07a3c935ef33092803f90c55ba19"; };
+        user_agent_parse = { name = "github.com/neodyme-labs/user_agent_parse"; version = "450380e8b6d048d71937014932ba6d4d56dd611d"; };
       };
     in
     { plugins
     , vendorSha256
     }:
     let
-      allPlugins = plugins builtins;
+      allPlugins = final.lib.flatten (plugins builtins);
       caddyPatchMain = final.lib.strings.concatMapStringsSep "\n"
         ({ name, version }: ''
           sed -i '/plug in Caddy modules here/a\\t_ "${name}"' cmd/caddy/main.go
@@ -54,7 +80,7 @@ rec {
       s3-proxy
       geolocation
     ];
-    vendorSha256 = "sha256-LRK8v2O36ZVnfADCZ+7tQhAz8wL0vc3UfXsEmfmuyRg=";
+    vendorSha256 = "sha256-oazBIYiRYH4k80xArxg71gAduDqiB82tOWbq1NhDT6I=";
   };
 
   # caddy with s3-browser plugin
@@ -66,5 +92,4 @@ rec {
     ];
     vendorSha256 = "sha256-aKnOah0mu5rM2WfAx9NSg8i2M3OzYXjZARWJs+ETxeI=";
   };
-
 }
