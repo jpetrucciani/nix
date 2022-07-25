7dbba60ecf9f5805dd274c0f33a4f472f7dad7ae
jacobi petrucciani <j@cobi.dev>
2022-07-25 17:21:25 -0400

    allow config override in import?

--- default.nix
+++ default.nix
@@ -5,5 +5,6 @@ with builtins;
     url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgs-json.rev}.tar.gz";
   }
 , overlays ? [ ]
+, config ? { }
 }:
-import nixpkgs { overlays = (import ./overlays.nix) ++ overlays; config = { }; }
+import nixpkgs { overlays = (import ./overlays.nix) ++ overlays; config = { allowUnfree = true; } // config; }
