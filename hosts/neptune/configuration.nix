4d3d2e4506e93e2a83d7c922c30c9e096c3cab16
jacobi petrucciani <j@cobi.dev>
2022-08-05 18:33:13 -0400

    fix reference

--- hosts/neptune/configuration.nix
+++ hosts/neptune/configuration.nix
@@ -154,7 +154,7 @@ in
 
   # https://github.com/NixOS/nixpkgs/issues/103158
   systemd.services.k3s.after = [ "network-online.service" "firewall.service" ];
-  systemd.services.k3s.serviceConfig.KillMode = lib.mkForce "control-group";
+  systemd.services.k3s.serviceConfig.KillMode = pkgs.lib.mkForce "control-group";
 
   # https://github.com/NixOS/nixpkgs/issues/98766
   boot.kernelModules = [ "br_netfilter" "ip_conntrack" "ip_vs" "ip_vs_rr" "ip_vs_wrr" "ip_vs_sh" "overlay" ];
