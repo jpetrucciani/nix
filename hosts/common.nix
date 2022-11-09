b6605318c41f4cc1864c3fced56d18d26e99dbe4
jacobi petrucciani <j@cobi.dev>
2022-11-08 23:49:04 -0500

    update sysctl options

--- hosts/common.nix
+++ hosts/common.nix
@@ -45,10 +45,10 @@ in
   extraGroups = [ "wheel" "networkmanager" "docker" "podman" ];
 
   sysctl_opts = {
-    "fs.inotify.max_user_watches" = "1048576";
-    "fs.inotify.max_queued_events" = "1048576";
-    "fs.inotify.max_user_instances" = "1048576";
-    "net.core.rmem_max" = "2500000";
+    "fs.inotify.max_user_watches" = 1048576;
+    "fs.inotify.max_queued_events" = 1048576;
+    "fs.inotify.max_user_instances" = 1048576;
+    "net.core.rmem_max" = 2500000;
   };
 
   env = {
