884e31b050bf2525eed53168513738b01602883e
jacobi petrucciani <j@cobi.dev>
2022-11-24 21:33:57 -0500

    update gitlab runner, update traefik

--- mods/pog/hex/k8s/traefik.nix
+++ mods/pog/hex/k8s/traefik.nix
@@ -6,13 +6,13 @@ let
     defaults = {
       name = "traefik";
       namespace = "traefik";
-      version = "20.5.1";
-      sha256 = "0whwfpiw9j0cl30s6p19kv5zg35f6m54223zby9vx78595s2kw6k";
+      version = "20.5.2";
+      sha256 = "04q0hkm9a0l53lacmb2dlicphv69gb9fc1ybfbbd4y97zy12iadg";
     };
     version = rec {
       _v = v: s: args: chart (args // { version = v; sha256 = s; });
-      latest = v20-4-1;
-      v20-5-1 = _v defaults.version defaults.sha256;
+      latest = v20-5-2;
+      v20-5-2 = _v defaults.version defaults.sha256;
       v20-4-1 = _v "20.4.1" "1mpcmrs6jny1p2r4v15h5q7b4k3v0cj2p5zjq6mvah0988wav3jn";
       v20-3-1 = _v "20.3.1" "1nrkh80qafmnwl00j16n04h737jgpd30yxr9r2g89l83p6a4v9xq";
       v20-2-1 = _v "20.2.1" "0yhnhcvp0pjfc4qsgsi4b9hzxavnaigazyyc4874jd5xs3z4gwmf";
@@ -129,10 +129,37 @@ let
         ---
         ${toYAML (setup args)}
       '';
-      setup = { name, spec, kind ? "Middleware", apiVersion ? "traefik.containo.us/v1alpha1" }: {
+      setup = { name, spec, kind ? "Middleware", apiVersion ? "traefik.containo.us/v1alpha1", extraSpec ? { } }: {
         inherit kind apiVersion spec;
         metadata = {
           inherit name;
+        };
+      } // extraSpec;
+      _ = {
+        default_index = { name ? "default-index", extraSpec ? { } }: setup {
+          inherit name extraSpec;
+          spec = {
+            replacePathRegex = {
+              regex = "^/$";
+              replacement = "/index.html";
+            };
+          };
+        };
+        add_prefix = { prefix, name ? "add-prefix", extraSpec ? { } }: setup {
+          inherit name extraSpec;
+          spec = {
+            addPrefix = {
+              inherit prefix;
+            };
+          };
+        };
+        strip_prefix = { prefixes, name ? "strip-prefix", extraSpec ? { } }: setup {
+          inherit name extraSpec;
+          spec = {
+            stripPrefix = {
+              inherit prefixes;
+            };
+          };
         };
       };
     };
