8b68bba79ad8333cc3e8b922da7d9bf93c63d2db
jacobi petrucciani <j@cobi.dev>
2022-12-27 15:13:16 -0500

    add falconn lib

---
+++ pkgs/falconn.nix
@@ -0,0 +1,32 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, ...
+}:
+
+stdenv.mkDerivation rec {
+  pname = "falconn";
+  version = "1.3.1";
+
+  src = fetchFromGitHub {
+    owner = "falconn-lib";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-kz4w3uW3Y45ov7g86MPA3x2WlvBP8EKLVhqeHDKiemk=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/include
+    cp -r ./src/include/falconn $out/include/.
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/FALCONN-LIB/FALCONN";
+    description = "Header only FAst Lookups of Cosine and Other Nearest Neighbors";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+  };
+
+  # This is a header-only library, no point in hydra building it!
+  hydraPlatforms = [ ];
+}
