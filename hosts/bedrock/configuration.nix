e7f425ee5962be1ace3e784adbc669d3c257a058
jacobi petrucciani <j@cobi.dev>
2023-01-31 00:00:15 -0500

    add trusted cidr blocks for postgres

--- hosts/bedrock/configuration.nix
+++ hosts/bedrock/configuration.nix
@@ -62,6 +62,8 @@ in
         local all all trust
         host all all 127.0.0.1/32 trust
         host all all ::1/128 trust
+        host all all 100.64.0.0/10 trust
+        host all all 192.168.69.0/24 trust
       '';
     };
   } // common.services;
