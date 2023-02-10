89dc3148163ee0cd4919ff9959d76d0878f858e1
jacobi petrucciani <j@cobi.dev>
2023-02-10 18:40:14 -0500

    update openssh server settings

--- hosts/common.nix
+++ hosts/common.nix
@@ -308,27 +308,27 @@ in
     openssh = {
       enable = true;
       settings = {
-        permitRootLogin = "no";
-        passwordAuthentication = false;
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KexAlgorithms = [
+          "curve25519-sha256"
+          "curve25519-sha256@libssh.org"
+        ];
+        Ciphers = [
+          "chacha20-poly1305@openssh.com"
+          "aes256-gcm@openssh.com"
+          # "aes128-gcm@openssh.com"
+          "aes256-ctr"
+          # "aes192-ctr"
+          # "aes128-ctr"
+        ];
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
+        X11Forwarding = true;
       };
-      forwardX11 = true;
-      kexAlgorithms = [
-        "curve25519-sha256"
-        "curve25519-sha256@libssh.org"
-      ];
-      ciphers = [
-        "chacha20-poly1305@openssh.com"
-        "aes256-gcm@openssh.com"
-        # "aes128-gcm@openssh.com"
-        "aes256-ctr"
-        # "aes192-ctr"
-        # "aes128-ctr"
-      ];
-      macs = [
-        "hmac-sha2-512-etm@openssh.com"
-        "hmac-sha2-256-etm@openssh.com"
-        "umac-128-etm@openssh.com"
-      ];
     };
   };
 
