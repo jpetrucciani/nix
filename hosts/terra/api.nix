42df8e1171d0b3c9e67324a9adcb952856dc41e1
jacobi petrucciani <j@cobi.dev>
2023-02-19 18:20:29 -0500

    fixup api service

--- hosts/terra/api.nix
+++ hosts/terra/api.nix
@@ -7,7 +7,7 @@ in
     wantedBy = [ "multi-user.target" ];
     script = ''
       cd /home/jacobi/dev/api
-      ${environment}/bin/uvicorn api:API \
+      ${environment}/bin/uvicorn api.api:API \
         --host 0.0.0.0 \
         --port 10000
     '';
