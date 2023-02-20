884963c660d564311f5480b18a2e0ea6151a8078
jacobi petrucciani <j@cobi.dev>
2023-02-20 14:06:13 -0500

    add lastresort cli tool

--- mods/pkgs/cli.nix
+++ mods/pkgs/cli.nix
@@ -445,4 +445,30 @@ with prev;
       })
     { };
 
+  lastresort = prev.callPackage
+    ({ lib, stdenv, fetchFromGitHub, rustPlatform }:
+      let
+        pname = "lastresort";
+        version = "0.4.0";
+      in
+      rustPlatform.buildRustPackage rec {
+        inherit pname version;
+
+        src = fetchFromGitHub {
+          owner = "ctsrc";
+          repo = "Base256";
+          rev = "v${version}";
+          sha256 = "sha256-wwwm7x42Fk7Hsf1rE+dKLQJGTkmZnbFGDl5OX3gJ1rU=";
+        };
+
+        cargoSha256 = "sha256-tt5B8jt3DSb7LWCCDWITpe9XD/EmFbGubUmlysFqRuM=";
+
+        meta = with lib; {
+          description = "Encode and decode data in base 256 easily typed words";
+          license = licenses.isc;
+          maintainers = with maintainers; [ jpetrucciani ];
+        };
+      })
+    { };
+
 }
