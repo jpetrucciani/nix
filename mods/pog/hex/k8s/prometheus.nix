c9f3bf6a8528109bf73f80e2e1147abbb7dd3eee
jacobi petrucciani <j@cobi.dev>
2023-03-07 09:12:28 -0500

    add path and timeout to PodMonitoring crd

--- mods/pog/hex/k8s/prometheus.nix
+++ mods/pog/hex/k8s/prometheus.nix
@@ -1,26 +1,36 @@
 { hex, ... }:
 let
-  inherit (hex) ifNotEmptyList toYAML;
+  inherit (hex) ifNotEmptyList ifNotNull toYAML;
 in
 {
   gmp = {
-    pod_monitoring = { name, port, matchLabels, namespace ? "default", interval ? "30s", metricRelabeling ? [ ] }: (toYAML {
-      apiVersion = "monitoring.googleapis.com/v1";
-      kind = "PodMonitoring";
-      metadata = {
-        inherit name namespace;
-      };
-      spec = {
-        endpoints = [
-          {
-            inherit interval port;
-            ${ifNotEmptyList metricRelabeling "metricRelabeling"} = metricRelabeling;
-          }
-        ];
-        selector = {
-          inherit matchLabels;
+    pod_monitoring =
+      { name
+      , port
+      , matchLabels
+      , path ? "/metrics"
+      , timeout ? null
+      , namespace ? "default"
+      , interval ? "30s"
+      , metricRelabeling ? [ ]
+      }: (toYAML {
+        apiVersion = "monitoring.googleapis.com/v1";
+        kind = "PodMonitoring";
+        metadata = {
+          inherit name namespace;
         };
-      };
-    });
+        spec = {
+          endpoints = [
+            {
+              inherit interval port path;
+              ${ifNotEmptyList metricRelabeling "metricRelabeling"} = metricRelabeling;
+              ${ifNotNull timeout "timeout"} = timeout;
+            }
+          ];
+          selector = {
+            inherit matchLabels;
+          };
+        };
+      });
   };
 }
