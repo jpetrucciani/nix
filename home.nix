5e432a63ae77b26e5d7fc56cfd364c3427bae264
jacobi petrucciani <j@cobi.dev>
2023-03-18 22:25:55 -0400

    reduce key algos

--- home.nix
+++ home.nix
@@ -513,7 +513,7 @@ with pkgs.hax; {
         Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
         KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256
         MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
-        HostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-512-cert-v01@openssh.com
+        HostKeyAlgorithms ssh-ed25519,rsa-sha2-256,rsa-sha2-512
         ${optionalString isDarwin mac_meme}
       '';
   };
