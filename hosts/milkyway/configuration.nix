d0ecb64b791958af6e7aabf94b17228435d1f231
jacobi petrucciani <j@cobi.dev>
2023-03-22 16:43:16 -0400

    update CUDA_PATH and LD_LIBRARY_PATH

--- hosts/milkyway/configuration.nix
+++ hosts/milkyway/configuration.nix
@@ -90,8 +90,8 @@ in
     enableNvidia = true;
   };
 
-  systemd.services.docker.Environment.CUDA_PATH = CUDA_PATH;
-  systemd.services.docker.Environment.LD_LIBRARY_PATH = CUDA_LDPATH;
+  systemd.services.docker.serviceConfig.Environment.CUDA_PATH = CUDA_PATH;
+  systemd.services.docker.serviceConfig.Environment.LD_LIBRARY_PATH = CUDA_LDPATH;
 
   # nvidia? not needed for cuda memes?
   hardware.opengl.enable = true;
