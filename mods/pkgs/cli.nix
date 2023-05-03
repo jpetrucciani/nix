ecc84e67232eba2aec12f90160048fdc12c23f66
jacobi petrucciani <jacobi.petrucciani@medable.com>
2023-05-02 23:28:17 -0400

    move horcrux to new pkgs system

--- mods/pkgs/cli.nix
+++ mods/pkgs/cli.nix
@@ -36,31 +36,6 @@ in
     })
     { };
 
-  horcrux = prev.callPackage
-    ({ lib, buildGo120Module, fetchFromGitHub }:
-      buildGo120Module rec {
-        pname = "horcrux";
-        version = "0.3";
-
-        src = fetchFromGitHub {
-          owner = "jesseduffield";
-          repo = "horcrux";
-          rev = "v${version}";
-          sha256 = "sha256-F8zGdjGKVuL/y763a1ZqcdmziGx9PfzXU81RaN7nL+Q=";
-        };
-
-        vendorHash = null;
-
-        meta = with lib; {
-          inherit (src.meta) homepage;
-          description = "Split your file into encrypted fragments so that you don't need to remember a passcode";
-          license = licenses.mit;
-          maintainers = with maintainers; [ jpetrucciani ];
-        };
-      }
-    )
-    { };
-
   hunt = prev.callPackage
     ({ lib, fetchFromGitHub, rustPlatform }:
       let
