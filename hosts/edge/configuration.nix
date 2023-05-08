186a5f39b930fbaf1dcb1463a5a863960a487ebf
jacobi petrucciani <jacobi.petrucciani@medable.com>
2023-05-08 11:32:02 -0500

    add base of edge machine

---
+++ hosts/edge/configuration.nix
@@ -0,0 +1,61 @@
+{ config, flake, machine-name, pkgs, ... }:
+let
+  hostname = "edge";
+  common = import ../common.nix { inherit config flake machine-name pkgs; };
+in
+{
+  imports = [
+    "${common.home-manager}/nixos"
+    ./hardware-configuration.nix
+  ];
+
+  inherit (common) zramSwap swapDevices;
+
+  nix = common.nix // {
+    nixPath = [
+      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+      "nixos-config=/home/jacobi/cfg/hosts/${hostname}/configuration.nix"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
+  };
+
+  home-manager.users.jacobi = common.jacobi;
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernel.sysctl = { } // common.sysctl_opts;
+    tmp.useTmpfs = true;
+  };
+
+  environment.variables = {
+    NIX_HOST = hostname;
+    NIXOS_CONFIG = "/home/jacobi/cfg/hosts/${hostname}/configuration.nix";
+  };
+
+  time.timeZone = common.timeZone;
+
+  networking.hostName = hostname;
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
+  networking.firewall.enable = false;
+
+  users.users.root.hashedPassword = "!";
+  users.mutableUsers = false;
+  users.users.jacobi = {
+    inherit (common) extraGroups;
+    isNormalUser = true;
+    passwordFile = "/etc/passwordFile-jacobi";
+
+    openssh.authorizedKeys.keys = with common.pubkeys; [ ] ++ usual;
+  };
+
+  services = { } // common.services;
+  virtualisation.docker.enable = true;
+
+  system.stateVersion = "22.11";
+  security.sudo = common.security.sudo;
+  programs.command-not-found.enable = false;
+}
---
+++ hosts/edge/hardware-configuration.nix
@@ -0,0 +1,26 @@
+{ config, lib, ... }:
+
+{
+  imports = [ ];
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "mptspi" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/06e6bd94-a5e4-4596-9cf9-8791687c06cb";
+    fsType = "ext4";
+  };
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens160.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
