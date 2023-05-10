2f2de22d98de92926f8482fdf9f4d49725ec1a64
jacobi petrucciani <j@cobi.dev>
2023-05-10 17:02:43 -0400

    move katafygio to new pkgs, fixup hashes

--- mods/pkgs/k8s.nix
+++ mods/pkgs/k8s.nix
@@ -1,37 +1,6 @@
 final: prev:
 with prev;
 {
-  katafygio = prev.callPackage
-    ({ lib, buildGo120Module, fetchFromGitHub }:
-      buildGo120Module rec {
-        pname = "katafygio";
-        version = "0.8.3";
-
-        src = fetchFromGitHub {
-          owner = "bpineau";
-          repo = "katafygio";
-          rev = "v${version}";
-          sha256 = "sha256-fRMXRKr620l7Y6uaYur3LbCGgLeSJ27zEGK0Zq7LZEY=";
-        };
-
-        vendorHash = "sha256-4hf6OueNHkReXdn9RuO4G4Zrpghp45YkuEwmci4wjz8=";
-
-        ldflags = [
-          "-s"
-          "-w"
-          "-X github.com/bpineau/katafygio/cmd.version=${version}"
-        ];
-
-        meta = with lib; {
-          inherit (src.meta) homepage;
-          description = "Dump, or continuously backup Kubernetes objects as yaml files in git";
-          license = licenses.mit;
-          maintainers = with maintainers; [ jpetrucciani ];
-        };
-      }
-    )
-    { };
-
   goldilocks = prev.callPackage
     ({ lib, buildGo120Module, fetchFromGitHub }:
       buildGo120Module rec {
---
+++ pkgs/k8s/katafygio.nix
@@ -0,0 +1,27 @@
+{ lib, buildGo120Module, fetchFromGitHub }:
+buildGo120Module rec {
+  pname = "katafygio";
+  version = "0.8.3";
+
+  src = fetchFromGitHub {
+    owner = "bpineau";
+    repo = "katafygio";
+    rev = "v${version}";
+    sha256 = "sha256-0UjhkQeR+97OZRug85e/mfri5ZZW3KaNJyCHT+9/7s4=";
+  };
+
+  vendorHash = "sha256-641dqcjPXq+iLx8JqqOzk9JsKnmohqIWBeVxT1lUNWU=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/bpineau/katafygio/cmd.version=${version}"
+  ];
+
+  meta = with lib; {
+    inherit (src.meta) homepage;
+    description = "Dump, or continuously backup Kubernetes objects as yaml files in git";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+  };
+}
