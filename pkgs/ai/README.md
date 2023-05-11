9cfae7e07bf9f9ce114b3f01b0c207d9c591e9df
jacobi petrucciani <j@cobi.dev>
2023-05-11 16:24:48 -0400

    add llm

--- pkgs/ai/README.md
+++ pkgs/ai/README.md
@@ -22,6 +22,10 @@ This directory contains packages that are related to the new wave of AI/LLM popu
 
 [`llama.cpp`](https://github.com/ggerganov/llama.cpp) is a port of [Facebook's LLaMA](https://ai.facebook.com/blog/large-language-model-llama-meta-ai/) model in C/C++
 
+### [llm.nix](./llm.nix)
+
+[`llm`](https://github.com/rustformers/llm) is a port of [`llama.cpp`](https://github.com/ggerganov/llama.cpp) in rust!
+
 ### [whisper-cpp.nix](./whisper-cpp.nix)
 
 [`whisper.cpp`](https://github.com/ggerganov/whisper.cpp) is a port of [OpenAI's Whisper](https://openai.com/research/whisperg) model in C/C++
---
+++ pkgs/ai/llm.nix
@@ -0,0 +1,25 @@
+{ lib, stdenv, fetchFromGitHub, rustPlatform }:
+let
+  pname = "llm";
+  version = "0.1.1";
+in
+rustPlatform.buildRustPackage rec {
+  inherit pname version;
+
+  src = fetchFromGitHub {
+    owner = "rustformers";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-yvLz3u5hMD1z+HyNjaCHLh+KZl1DNvfUedGe8pr3ayM=";
+    fetchSubmodules = true;
+  };
+
+  cargoHash = "sha256-TuN729VwCGsRHjN6LHEATfRUMGYZuEWl8vi/qz7DAf4=";
+
+  meta = with lib; {
+    description = "Run inference for Large Language Models on CPU, with Rust";
+    homepage = "https://github.com/rustformers/llm";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+  };
+}
