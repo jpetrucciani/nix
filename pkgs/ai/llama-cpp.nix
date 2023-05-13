9d75e86e16b18189763d04d9773adeab540e926d
jacobi petrucciani <j@cobi.dev>
2023-05-13 10:25:12 -0400

    update llama-cpp

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -14,7 +14,7 @@ let
       ### Human:
     '';
   };
-  version = "master-905d87b";
+  version = "master-66841fd";
 in
 clangStdenv.mkDerivation rec {
   inherit version;
@@ -23,7 +23,7 @@ clangStdenv.mkDerivation rec {
     owner = "ggerganov";
     repo = name;
     rev = "refs/tags/${version}";
-    hash = "sha256-yfkMDAQnnlY2jfJHUqt3XqhA2hzA4NbNlxsJANGEHSo=";
+    hash = "sha256-NDEhVUPJKoiwIjJiHsjvofftHPbI660buX7JnKENkYY=";
   };
   cmakeFlags = lib.optionals (system == "aarch64-darwin") [
     "-DCMAKE_C_FLAGS=-D__ARM_FEATURE_DOTPROD=1"
