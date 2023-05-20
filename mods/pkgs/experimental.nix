205142d5840692caee7af11371322f30d52f2dc8
jacobi petrucciani <j@cobi.dev>
2023-05-20 11:21:19 -0400

    move ggml to experimental

--- mods/pkgs/experimental.nix
+++ mods/pkgs/experimental.nix
@@ -1,3 +1,47 @@
 final: prev:
 with prev;
-{ }
+{
+  ggml = prev.callPackage
+    ({ lib, system, darwin, stdenv, clangStdenv, fetchFromGitHub, cmake, git }:
+      let
+        inherit (stdenv) isAarch64 isDarwin;
+        osSpecific = with darwin.apple_sdk.frameworks; if isDarwin then ([ Accelerate ] ++ (if !isAarch64 then [ CoreGraphics CoreVideo ] else [ ])) else [ ];
+      in
+      clangStdenv.mkDerivation rec {
+        name = "ggml";
+        version = "0.0.0";
+
+        src = fetchFromGitHub {
+          owner = "ggerganov";
+          repo = name;
+          rev = "2a75bd43113702fe096c75408b300d106be90a58";
+          hash = "sha256-3IgIes8x5yoHwcVJV67kizV95ZocrTd29bmrbKJBRzU=";
+        };
+        cmakeFlags = lib.optionals (system == "aarch64-darwin") [
+          "-DCMAKE_C_FLAGS=-D__ARM_FEATURE_DOTPROD=1"
+        ];
+
+        buildPhase = ''
+          cmake .
+          cmake --build . --config Release
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          mv ./bin/{mpt,replit,starcoder,whisper} $out/bin/.
+        '';
+
+        buildInputs = osSpecific;
+        nativeBuildInputs = [ cmake git ];
+
+        meta = with lib; {
+          description = "Tensor library for machine learning";
+          homepage = "https://github.com/ggerganov/ggml";
+          mainProgram = "mpt";
+          license = licenses.mit;
+          maintainers = with maintainers; [ jpetrucciani ];
+        };
+      }
+    )
+    { };
+}
