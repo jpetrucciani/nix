d91992312d4db3c0b8b7775a2abe47445c6999cc
jacobi petrucciani <jacobi.petrucciani@medable.com>
2023-05-31 21:49:35 -0400

    gcsproxy: 0.3.2 -> 0.4.0
    Diff: https://github.com/daichirata/gcsproxy/compare/v0.3.2...v0.4.0

--- pkgs/cloud/gcsproxy.nix
+++ pkgs/cloud/gcsproxy.nix
@@ -1,13 +1,13 @@
 { lib, buildGo120Module, fetchFromGitHub, ... }:
 buildGo120Module rec {
   pname = "gcsproxy";
-  version = "0.3.2";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "daichirata";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-yeAN2pFgakgqTM4/sq9sgVBJi2zL3qamHsKN3s+ntds=";
+    sha256 = "sha256-lMOGaNkNCdWJbNU24OfHJIafRbK7dSi8B3s5mP9icgg=";
   };
 
   vendorHash = "sha256-Wsa9zPFE4q9yBxflovzkrzn0Jq1a4zlxc5jJOsl7HDQ=";
