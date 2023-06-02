18d3308e601d614634e0c86b73b840c1a0e513c7
jacobi petrucciani <j@cobi.dev>
2023-06-02 13:33:46 -0400

    add falcon-cpp

---
+++ pkgs/ai/falcon-cpp.nix
@@ -0,0 +1,31 @@
+{ lib, darwin, stdenv, clangStdenv, fetchFromGitHub }:
+let
+  inherit (stdenv) isAarch64 isDarwin;
+  osSpecific = with darwin.apple_sdk.frameworks; if isDarwin then ([ Accelerate CoreGraphics CoreVideo ]) else [ ];
+in
+clangStdenv.mkDerivation rec {
+  name = "falcon.cpp";
+  src = fetchFromGitHub {
+    owner = "nikisalli";
+    repo = name;
+    rev = "fcb95afe6a38366f6d1c7c4741cde5cead77107b";
+    hash = "sha256-Rdb9KPnNeQFH7NhcesvNZPDsLpzsfaBl7w/+c3QByzw=";
+  };
+  buildPhase = ''
+    make
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./main $out/bin/falcon
+    mv ./quantize $out/bin/falcon-quantize
+  '';
+  buildInputs = osSpecific;
+
+  meta = with lib; {
+    description = "c++ implementation of falcon";
+    homepage = "https://github.com/nikisalli/falcon.cpp";
+    mainProgram = "chat";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+  };
+}
