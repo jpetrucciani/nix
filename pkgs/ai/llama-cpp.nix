07fd84f0c35b32558cee5a36e45199c3a54f1f0c
jacobi petrucciani <j@cobi.dev>
2023-06-06 08:32:54 -0400

    update llama-cpp

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -3,7 +3,7 @@ let
   inherit (stdenv) isAarch64 isDarwin;
   apple_gpu = isDarwin && isAarch64;
   osSpecific = with darwin.apple_sdk_11_0.frameworks; if isDarwin then ([ Accelerate ] ++ (if apple_gpu then [ CoreML MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ] else [ ])) else [ ];
-  version = "master-9d0693b";
+  version = "master-2d43387";
 in
 clangStdenv.mkDerivation rec {
   inherit version;
@@ -12,7 +12,7 @@ clangStdenv.mkDerivation rec {
     owner = "ggerganov";
     repo = name;
     rev = "refs/tags/${version}";
-    hash = "sha256-FOThm84jsNXbIVjPVqifWbarT2Gn0CbTReFf+E9s+kg=";
+    hash = "sha256-b6aX5SjP3VbEdJEvnwIgf3BZK5KvKBwB52pfqy+r/II=";
   };
 
   postPatch =
