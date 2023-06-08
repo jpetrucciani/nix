1e69b1306ae8dfd0428b1c0ab1e75836501ba2ea
jacobi petrucciani <j@cobi.dev>
2023-06-07 22:49:22 -0400

    python311Packages.gradio: 3.33.1 -> 3.34.0

--- mods/python/ai.nix
+++ mods/python/ai.nix
@@ -1237,7 +1237,7 @@ rec {
 
   gradio = buildPythonPackage rec {
     pname = "gradio";
-    version = "3.33.1";
+    version = "3.34.0";
     disabled = pythonOlder "3.7";
     format = "pyproject";
 
@@ -1245,7 +1245,7 @@ rec {
     # and its releases are also more frequent than github tags
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-55AzaSYGuUXcwryPOKPNLDoazTcIc2UhXBuwCtf3aqM=";
+      sha256 = "sha256-/X+nJX/8dJ+dx8KX66VU6qHlrNGl+clzJQsggJMtakE=";
     };
 
     nativeBuildInputs = [
