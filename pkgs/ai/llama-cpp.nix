52632902233c5290b2177bccc9a5eb8653ccbbfe
jacobi petrucciani <j@cobi.dev>
2023-06-08 08:09:10 -0400

    update llama-cpp

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-5c64a09";
+  version = "master-b50b570";
 in
 clangStdenv.mkDerivation rec {
   inherit version;
@@ -16,7 +16,7 @@ clangStdenv.mkDerivation rec {
     owner = "ggerganov";
     repo = name;
     rev = "refs/tags/${version}";
-    hash = "sha256-DSRQHkmrb1RT83YOJwlEFEX1Qdz/4gHj91rlW4Jc5Ks=";
+    hash = "sha256-GA6uVrClDJCYaQKLCH2nvI8+ArnNhIXbKd8v9Ks2N7M=";
   };
 
   postPatch =
