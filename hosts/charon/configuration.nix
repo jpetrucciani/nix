a28288818cdb116bed86d7032f2872939400ed7b
jacobi petrucciani <j@cobi.dev>
2023-06-14 10:52:26 -0400

    first commit of charon github runner

--- hosts/charon/configuration.nix
+++ hosts/charon/configuration.nix
@@ -1,13 +1,33 @@
-{ config, flake, machine-name, pkgs, ... }:
+{ config, flake, machine-name, pkgs, lib, ... }:
 let
+  inherit (lib.attrsets) mapAttrs' nameValuePair;
+  inherit (lib.trivial) flip;
+
+  mapAttrValues = f: builtins.mapAttrs (_: v: f v);
+  forAttrValues = flip mapAttrValues;
+
   hostname = "charon";
   common = import ../common.nix { inherit config flake machine-name pkgs; };
   configPath = "/Users/jacobi/cfg/hosts/${hostname}/configuration.nix";
   username = "jacobi";
+
+  github-runners-fork = builtins.fetchTree {
+    type = "github";
+    owner = "DavHau";
+    repo = "nix-darwin";
+    ref = "github-runners";
+    narHash = "sha256-z9PpdTZRRaAXM6eFznIgbNCRp9jQoIAQ67k0YDxvn/A=";
+  };
+  runner-defaults = {
+    url = "https://github.com/jpetrucciani/nix";
+    extraLabels = [ "nix" "m1" ];
+    extraPackages = [ ];
+  };
 in
 {
   imports = [
     "${common.home-manager}/nix-darwin"
+    "${github-runners-fork}/modules/services/github-runners"
   ];
 
   home-manager.users.jacobi = common.jacobi;
@@ -30,7 +50,6 @@ in
     ];
   };
 
-
   system.stateVersion = 4;
   nix = common.nix // {
     useDaemon = true;
@@ -39,4 +58,29 @@ in
       "darwin-config=${configPath}"
     ];
   };
+
+  services.github-runners = mapAttrs' (n: nameValuePair "runner-${n}") {
+    jpetrucciani-nix = runner-defaults // {
+      extraPackages = with pkgs; [ gh cachix ];
+      tokenFile = "/etc/default/gh.token";
+    };
+  };
+
+  launchd.daemons = forAttrValues config.services.github-runners (cfg: {
+    # daemon path fixes copied from the nixos module
+    path = with pkgs;[
+      bash
+      coreutils
+      git
+      gnutar
+      gzip
+    ] ++ [
+      (
+        # allow x64 runners
+        if cfg.package != pkgsx86_64Darwin.github-runner
+        then config.nix.package
+        else pkgsx86_64Darwin.nix
+      )
+    ] ++ cfg.extraPackages;
+  });
 }
