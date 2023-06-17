40a810abd5c61ab7ea6a2230bb6493bb92e44a84
jacobi petrucciani <j@cobi.dev>
2023-06-17 00:15:29 -0400

    add headscale-ui package

---
+++ pkgs/cloud/headscale-ui.nix
@@ -0,0 +1,26 @@
+{ fetchFromGitHub, buildNpmPackage }:
+let
+  pname = "headscale-ui";
+  version = "0.0.1";
+  src = fetchFromGitHub {
+    owner = "gurucomputing";
+    repo = "headscale-ui";
+    rev = "63041fd673d81da56e60d2b528a4991981eab746";
+    hash = "sha256-pz7oDRfBf/dN+PMEqbMe+es6deQ4QP3pC191ASlyV7U=";
+  };
+in
+buildNpmPackage {
+  inherit src version;
+  name = pname;
+  npmDepsHash = "sha256-MePNbOPSe5wB8/6T3DLs+4+Qlr8f+7cCPs301il7iX8=";
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p $out
+    npm run build
+    mv ./build $out/dist
+    runHook postBuild
+  '';
+  makeCacheWritable = true;
+  dontFixup = true;
+  dontNpmBuild = true;
+}
