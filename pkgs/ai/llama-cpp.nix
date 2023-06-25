49559880182ec87aa2fd75f3d036ed7614184366
jacobi petrucciani <j@cobi.dev>
2023-06-25 08:01:54 -0400

    llama-cpp: master-f2c754e -> master-c2a08f8
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-f2c754e...master-c2a08f8

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-f2c754e";
+  version = "master-c2a08f8";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -17,7 +17,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-SIFvIhMP0kvcMB9S3EKRkKAfFm8h1WtNuU1EEaQbVak=";
+    hash = "sha256-tOUxnNPqe9NFN3w1EJCLHvpOI3UZz1uvsbko7wqCteI=";
   };
 
   postPatch =
