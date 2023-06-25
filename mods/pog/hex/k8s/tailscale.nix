51bd7858064d7785595c5b5058777b172cce114b
jacobi petrucciani <j@cobi.dev>
2023-06-24 21:07:47 -0400

    update tailscale image tag, update cloudsql image tag

--- mods/pog/hex/k8s/tailscale.nix
+++ mods/pog/hex/k8s/tailscale.nix
@@ -17,9 +17,9 @@ let
         memory = "2Gi";
       };
       tailscale_image_base = "ghcr.io/tailscale/tailscale";
-      tailscale_image_tag = "v1.42.0";
+      tailscale_image_tag = "v1.44.0";
       cloudsql_image_base = "gcr.io/cloudsql-docker/gce-proxy";
-      cloudsql_image_tag = "1.33.7";
+      cloudsql_image_tag = "1.33.8";
 
       tags = [ "k8s" "proxy" ];
       cidr = "100.64.0.0/10";
