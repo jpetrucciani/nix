6b9a15e5f5ac4bb9f0be887e2b4cbba6b1b69bb4
jacobi petrucciani <j@cobi.dev>
2023-07-04 10:51:09 -0400

    llama-cpp: master-698efad -> master-acc111c
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-698efad...master-acc111c

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-698efad";
+  version = "master-acc111c";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -17,7 +17,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-uYvdA50rqaHaxFDtUJENwBJCkyCMI3fR3dBUdp+wEgY=";
+    hash = "sha256-0WQOe7fy8mZ214WSMIVrouPjISi/5bRdcFbVg2tuQLs=";
   };
 
   postPatch =
