f32ccc41428f66ee91435be9a809c61f95264e4b
jacobi petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2023-07-05 09:42:21 -0400

    add work mode for nix-darwin (#1272)

--- hosts/common_darwin.nix
+++ hosts/common_darwin.nix
@@ -1,121 +1,126 @@
-{ pkgs
-, ...
-}:
+{ lib, config, ... }:
 let
-  inherit (pkgs.lib.lists) subtractLists;
+  inherit (lib.lists) subtractLists;
+  inherit (lib) mkEnableOption;
+  inherit (config.conf) work;
 in
 {
-  system = {
-    defaults = {
-      NSGlobalDomain = {
-        AppleKeyboardUIMode = 3;
-        ApplePressAndHoldEnabled = false;
-        InitialKeyRepeat = 10;
-        KeyRepeat = 1;
-        NSAutomaticCapitalizationEnabled = false;
-        NSAutomaticDashSubstitutionEnabled = false;
-        NSAutomaticPeriodSubstitutionEnabled = false;
-        NSAutomaticQuoteSubstitutionEnabled = false;
-        NSAutomaticSpellingCorrectionEnabled = false;
-        NSNavPanelExpandedStateForSaveMode = true;
-        NSNavPanelExpandedStateForSaveMode2 = true;
-        _HIHideMenuBar = false;
-      };
-
-      screencapture = { location = "/tmp"; };
-      dock = {
-        autohide = true;
-        mru-spaces = false;
-        orientation = "left";
-        showhidden = true;
-      };
-
-      finder = {
-        AppleShowAllExtensions = true;
-        QuitMenuItem = true;
-        FXEnableExtensionChangeWarning = false;
-      };
-
-      trackpad = {
-        Clicking = true;
-        TrackpadThreeFingerDrag = true;
-      };
-    };
-
-    keyboard = {
-      enableKeyMapping = true;
-      remapCapsLockToControl = true;
-    };
+  options.conf.work = {
+    enable = mkEnableOption "work";
   };
+  config = {
+    system = {
+      defaults = {
+        NSGlobalDomain = {
+          AppleKeyboardUIMode = 3;
+          ApplePressAndHoldEnabled = false;
+          InitialKeyRepeat = 10;
+          KeyRepeat = 1;
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+          NSNavPanelExpandedStateForSaveMode = true;
+          NSNavPanelExpandedStateForSaveMode2 = true;
+          _HIHideMenuBar = false;
+        };
 
-  programs.bash.enable = true;
+        screencapture = { location = "/tmp"; };
+        dock = {
+          autohide = true;
+          mru-spaces = false;
+          orientation = "left";
+          showhidden = true;
+        };
 
-  homebrew =
-    let
-      casks = rec {
-        fonts = [
-          "font-caskaydia-cove-nerd-font"
-          "font-fira-code-nerd-font"
-          "font-hasklug-nerd-font"
-        ];
-        fun = [
-          "spotify"
-          "steam"
-        ];
-        work = [
-          "1password"
-          "dropbox"
-          "robo-3t"
-          "slack"
-          "xca"
-        ];
-        comms = [
-          "discord"
-        ];
-        util = [
-          "alfred"
-          "docker"
-          "insomnia"
-          "karabiner-elements"
-          "keybase"
-          "macfuse"
-          "notion"
-          "parsec"
-          "qlvideo"
-          "raycast"
-          "rectangle"
-          "utm"
-        ];
-        all = fonts ++ fun ++ work ++ comms ++ util;
-        all_personal = subtractLists work all;
-        all_work = subtractLists fun all;
+        finder = {
+          AppleShowAllExtensions = true;
+          QuitMenuItem = true;
+          FXEnableExtensionChangeWarning = false;
+        };
+
+        trackpad = {
+          Clicking = true;
+          TrackpadThreeFingerDrag = true;
+        };
       };
-    in
-    {
-      enable = true;
-      taps = [
-        "homebrew/cask"
-        "homebrew/cask-drivers"
-        "homebrew/cask-fonts"
-        "homebrew/cask-versions"
-        "homebrew/core"
-        "homebrew/services"
-      ];
-      brews = [
-        "readline"
-        "sshfs"
-        "qemu"
-      ];
-      onActivation = {
-        autoUpdate = true;
-        cleanup = "zap";
-        upgrade = true;
+
+      keyboard = {
+        enableKeyMapping = true;
+        remapCapsLockToControl = true;
       };
-      casks = casks.all_personal;
-      masApps = {
-        Wireguard = 1451685025;
-        Poolside = 1514817810;
-      };
-      extraConfig = "";
     };
+
+    programs.bash.enable = true;
+
+    homebrew =
+      let
+        casks = rec {
+          fonts = [
+            "font-caskaydia-cove-nerd-font"
+            "font-fira-code-nerd-font"
+            "font-hasklug-nerd-font"
+          ];
+          fun = [
+            "spotify"
+            "steam"
+          ];
+          work = [
+            "1password"
+            "dropbox"
+            "robo-3t"
+            "slack"
+            "xca"
+          ];
+          comms = [
+            "discord"
+          ];
+          util = [
+            "alfred"
+            "docker"
+            "insomnia"
+            "karabiner-elements"
+            "keybase"
+            "macfuse"
+            "notion"
+            "parsec"
+            "qlvideo"
+            "raycast"
+            "rectangle"
+            "utm"
+          ];
+          all = fonts ++ fun ++ work ++ comms ++ util;
+          all_personal = subtractLists work all;
+          all_work = subtractLists fun all;
+        };
+      in
+      {
+        enable = true;
+        taps = [
+          "homebrew/cask"
+          "homebrew/cask-drivers"
+          "homebrew/cask-fonts"
+          "homebrew/cask-versions"
+          "homebrew/core"
+          "homebrew/services"
+        ];
+        brews = [
+          "readline"
+          "sshfs"
+          "qemu"
+        ];
+        onActivation = {
+          autoUpdate = true;
+          cleanup = "zap";
+          upgrade = true;
+        };
+        casks = if work.enable then casks.all_work else casks.all_personal;
+        masApps = {
+          Wireguard = 1451685025;
+          Poolside = 1514817810;
+        };
+        extraConfig = "";
+      };
+  };
 }
