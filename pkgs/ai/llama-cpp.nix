005d82c360293abc7d63d7117e331c1814ca4f9c
jacobi petrucciani <j@cobi.dev>
2023-07-10 13:09:50 -0400

    llama-cpp: master-db4047a -> master-5656d10
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-db4047a...master-5656d10

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-db4047a";
+  version = "master-5656d10";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -17,7 +17,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-4zfYlztxDD93gjlBR3bx3fpGcI/SOmNrPzriGu3vFBM=";
+    hash = "sha256-4cec/iVClkt29yUQMp6uLvrsft+cxMRM6ODRikIXCdI=";
   };
 
   postPatch =
