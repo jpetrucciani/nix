b623f259cf2dbd86575139b54d7fe09dae697dec
jacobi petrucciani <j@cobi.dev>
2023-07-09 20:27:46 -0400

    update llm

--- pkgs/ai/llm.nix
+++ pkgs/ai/llm.nix
@@ -1,13 +1,13 @@
-{ lib, stdenv, darwin, fetchFromGitHub, rustPlatform }:
+{ lib, stdenv, darwin, fetchFromGitHub, rustPlatform, openssl, pkg-config }:
 let
   inherit (stdenv) isAarch64 isDarwin;
   isM1 = isDarwin && isAarch64;
   osSpecific =
-    if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate ]
+    if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
   pname = "llm";
-  version = "0.1.1";
+  version = "0.0.0";
 in
 rustPlatform.buildRustPackage rec {
   inherit pname version;
@@ -15,13 +15,16 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "rustformers";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-yvLz3u5hMD1z+HyNjaCHLh+KZl1DNvfUedGe8pr3ayM=";
+    rev = "7f13bb90f678e2bdf70d221f1b790fab55cb4d7f";
+    sha256 = "sha256-h4PSMMa7wY50fffcgkvpah5ACJWhekrwHyTq2hzwtbo=";
     fetchSubmodules = true;
   };
-  buildInputs = osSpecific;
+  buildInputs = [
+    openssl
+  ] ++ osSpecific;
+  nativeBuildInputs = [ pkg-config ];
 
-  cargoHash = "sha256-TuN729VwCGsRHjN6LHEATfRUMGYZuEWl8vi/qz7DAf4=";
+  cargoHash = "sha256-xDloCgOv12BcueEx/kpM/2810Us7rVk8RnIFnaCjW2M=";
 
   meta = with lib; {
     description = "Run inference for Large Language Models on CPU, with Rust";
