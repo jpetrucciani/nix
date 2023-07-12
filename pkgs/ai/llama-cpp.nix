12a283bf7d090169795ff6c48fccdcb88bb55cb2
jacobi petrucciani <j@cobi.dev>
2023-07-12 10:38:35 -0400

    llama-cpp: master-c9c74b4 -> master-f7d278f
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-c9c74b4...master-f7d278f

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-c9c74b4";
+  version = "master-f7d278f";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -17,7 +17,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-EAk9LwqMyYuc4gqlPh99a8w5o00Y0FN0yClY0AcwXSs=";
+    hash = "sha256-MNCSMY5tn/zE/9QmLAYgwzsV1T2/GyHt8MGEyQVNMZE=";
   };
 
   postPatch =
