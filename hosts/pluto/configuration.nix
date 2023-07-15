d26186e8793c09ecd7681ab1594d76d520ea5f53
jacobi petrucciani <j@cobi.dev>
2023-07-14 23:32:58 -0400

    fix pluto config

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -19,7 +19,7 @@ in
   environment.variables = {
     NIX_HOST = hostname;
     NIXDARWIN_CONFIG = configPath;
-  } // common.env;
+  };
   environment.darwinConfig = configPath;
 
   users.users.jacobi = {
