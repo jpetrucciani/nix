28095cbc42b1c6f612a57ec8370aa815dc5bf536
jacobi petrucciani <j@cobi.dev>
2023-07-20 23:39:30 -0500

    llama-cpp: master-b1f4290 -> master-fff0e0e
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-b1f4290...master-fff0e0e

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-b1f4290";
+  version = "master-fff0e0e";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -17,7 +17,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-ZyExNnFQCvC03FS0UVbt33AAuD6IghNXpicIU1nhI3I=";
+    hash = "sha256-iYQZRSorAucYG+iqzP9R1D0EiYEL0DnOHa8Ng2u6BXE=";
   };
 
   postPatch =
