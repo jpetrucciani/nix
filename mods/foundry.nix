61f10eff72adbb0b60ee50ac84fb4a824ce93794
jacobi petrucciani <j@cobi.dev>
2023-07-24 14:25:29 -0400

    RIP foundry v1

--- mods/foundry.nix
+++ mods/foundry.nix
@@ -1,7 +1,6 @@
 { pkgs }:
 let
-  inherit (pkgs.lib) concatStringsSep genList optionals;
-  inherit (pkgs.writers) writeBashBin;
+  inherit (pkgs.lib) concatStringsSep optionals;
   nixconf =
     { substituters ? [ "https://jacobi.cachix.org" ]
     , trusted-public-keys ? [ "jacobi.cachix.org-1:JJghCz+ZD2hc9BHO94myjCzf4wS3DeBLKHOz3jCukMU=" ]
@@ -12,15 +11,6 @@ let
       substituters = https://cache.nixos.org/ ${concatStringsSep " " substituters}
       trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= ${concatStringsSep " " trusted-public-keys}
     '';
-  passwd = ''
-    root:x:0:0::/root:/bin/bash
-    ${concatStringsSep "\n" (genList (i: "nixbld${toString (i+1)}:x:${toString (i+30001)}:30000::/var/empty:/run/current-system/sw/bin/nologin") 32)}
-  '';
-  group = ''
-    root:x:0:
-    nogroup:x:65534:
-    nixbld:x:30000:${concatStringsSep "," (genList (i: "nixbld${toString (i+1)}") 32)}
-  '';
   _base_pkgs = _pkgs: with _pkgs; [
     deadnix
     delta
@@ -161,89 +151,6 @@ let
       nixUid = toInt uid;
       nixGid = toInt gid;
     };
-
-  foundry =
-    { imageName
-    , paths
-    , command ? "bash"
-    , base_pkgs ? _base_pkgs
-    , env ? [ ]
-    , extraRootCommands ? ""
-    , registry ? "ghcr.io/jpetrucciani"
-    , workdir ? "/opt/foundry"
-    , author ? "j@cobi.dev"
-    , description ? "a foundry docker image built with nix"
-    , hostPkgs ? pkgs
-    , enableNix ? true
-    }:
-    let
-      name = "foundry-${imageName}";
-      deps = paths pkgs;
-      foundryImage =
-        hostPkgs.dockerTools.streamLayeredImage {
-          inherit name;
-          architecture = "amd64";
-          contents = pkgs.buildEnv {
-            inherit name;
-            paths = deps ++ (base_pkgs pkgs) ++ (with pkgs; [
-              bashInteractive
-              coreutils
-              curl
-              gnugrep
-              gnused
-              util-linux
-            ]) ++ (if enableNix then [ pkgs.nix ] else [ ]);
-          };
-          config = {
-            Env = [
-              "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              "USER=nobody"
-              "HOME=${workdir}"
-            ] ++ (if enableNix then [
-              "NIX_PAGER=cat"
-              "NIX_PATH=nixpkgs=${pkgs.path}"
-            ] else [ ]) ++ env;
-            Labels = {
-              "org.opencontainers.image.authors" = author;
-              "org.opencontainers.image.description" = description;
-            };
-            WorkingDir = workdir;
-            Cmd = if builtins.isString command then [ command ] else command;
-          };
-          enableFakechroot = true;
-          fakeRootCommands =
-            if enableNix then ''
-              mkdir -m 1777 -p /tmp /var/tmp
-              mkdir -p /etc/nix
-              echo '${nixconf}' >/etc/nix/nix.conf
-              echo '${passwd}' >/etc/passwd
-              echo '${group}' >/etc/group
-              ${extraRootCommands}
-            '' else null;
-          passthru = rec {
-            _raw_tag = "$(${raw_tag}/bin/raw_tag)";
-            raw_tag = writeBashBin "raw_tag" ''
-              echo "${registry}/${name}"
-            '';
-            _date_tag = "$(${date_tag}/bin/date_tag)";
-            date_tag = writeBashBin "date_tag" ''
-              echo "$(${raw_tag}/bin/raw_tag):$(${pkgs.coreutils}/bin/date +"%F")"
-            '';
-            build = writeBashBin "build" ''
-              ${foundryImage} | docker load
-            '';
-            pushToRegistry = writeBashBin "pushToRegistry" ''
-              ${foundryImage} |
-                ${pkgs.gzip}/bin/gzip --fast |
-                ${pkgs.skopeo}/bin/skopeo --insecure-policy copy docker-archive:/dev/stdin "docker://${_date_tag}"
-            '';
-            tagAsLatest = writeBashBin "tagAsLatest" ''
-              ${pkgs.skopeo}/bin/skopeo copy "docker://${_date_tag}" "docker://${_raw_tag}:latest"
-            '';
-          };
-        };
-    in
-    foundryImage;
 
   foundryNix = foundry_v2 {
     name = "nix";
