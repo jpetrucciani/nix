580072caf262f6134185c17d842ffbc494d7c5be
jacobi petrucciani <j@cobi.dev>
2023-07-26 10:50:08 -0400

    llama-cpp: master-eb542d3 -> master-5488fb7
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-eb542d3...master-5488fb7

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -7,7 +7,7 @@ let
     if isM1 then with darwin.apple_sdk_11_0.frameworks; [ Accelerate MetalKit MetalPerformanceShaders MetalPerformanceShadersGraph ]
     else if isDarwin then with darwin.apple_sdk.frameworks; [ Accelerate CoreGraphics CoreVideo ]
     else [ ];
-  version = "master-eb542d3";
+  version = "master-5488fb7";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -17,7 +17,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-mh2NzDnhCEhN1czemVhH8SViYVeKWIFESAWPga4sN3s=";
+    hash = "sha256-JBPfxEvzujTczvAC71G0/k04lIFJnLnCE1suJS7qCao=";
   };
 
   postPatch =
