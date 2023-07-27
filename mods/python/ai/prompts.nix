3dd6fa46a0b06ff4999916ff255fb175e577cb32
jacobi petrucciani <j@cobi.dev>
2023-07-27 14:39:35 -0400

    python311Packages.langchain: 0.0.244 -> 0.0.245
    Diff: https://github.com/hwchase17/langchain/compare/refs/tags/v0.0.244...v0.0.245
    
    Changelog: https://github.com/hwchase17/langchain/releases/tag/v0.0.245

--- mods/python/ai/prompts.nix
+++ mods/python/ai/prompts.nix
@@ -30,14 +30,14 @@ final: prev: with prev; rec {
 
   langchain = buildPythonPackage rec {
     pname = "langchain";
-    version = "0.0.244";
+    version = "0.0.245";
     format = "pyproject";
 
     src = pkgs.fetchFromGitHub {
       owner = "hwchase17";
       repo = pname;
       rev = "refs/tags/v${version}";
-      hash = "sha256-yiSkQ6dkJQxlDKqFKaCcAm+Fa/9FnXos4UYehMnrN1c=";
+      hash = "sha256-SWp1dYpAc0dMyRNnlkJQ39yOVVJP/dOVR1ljJNwWaQ8=";
     };
     sourceRoot = "source/libs/langchain";
 
