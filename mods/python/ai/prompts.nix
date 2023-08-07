5ebbcf85aca54e15651aa620c6c9860ce2162ea6
jacobi petrucciani <j@cobi.dev>
2023-08-07 13:59:06 -0400

    python311Packages.langsmith: 0.0.16 -> 0.0.19

--- mods/python/ai/prompts.nix
+++ mods/python/ai/prompts.nix
@@ -1,12 +1,12 @@
 final: prev: with prev; rec {
   langsmith = buildPythonPackage rec {
     pname = "langsmith";
-    version = "0.0.16";
+    version = "0.0.19";
     format = "pyproject";
 
     src = fetchPypi {
       inherit pname version;
-      hash = "sha256-BynfMN1bHJ0Mg2FLXTSMc1isGC1+MAPa+/y+24jLyLg=";
+      hash = "sha256-6Ros0QFFbi+NYBXJ6jcdZVbrYHKhsg1Hk0eYVRY64o8=";
     };
 
     nativeBuildInputs = [
