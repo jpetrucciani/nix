37bf4329018d60d67e10c1d7fb7e9f28d033717d
jacobi petrucciani <j@cobi.dev>
2023-08-08 09:13:17 -0400

    add bonk

---
+++ pkgs/cli/bonk.nix
@@ -0,0 +1,25 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "bonk";
+  version = "0.3.2";
+
+  src = fetchFromGitHub {
+    owner = "elliot40404";
+    repo = "bonk";
+    rev = "v${version}";
+    hash = "sha256-Y6Hia+B7kIvdvpuZwWGJBsn+pOBmMynXai4KWkNs4ck=";
+  };
+
+  cargoHash = "sha256-XphSjB49zFB3zXYpdjjcVRdTAW2Bvg91aZkxDLvFy3M=";
+
+  meta = with lib; {
+    description = "The blazingly fast touch alternative with a sprinkle of mkdir written in rust";
+    homepage = "https://github.com/elliot40404/bonk";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+  };
+}
