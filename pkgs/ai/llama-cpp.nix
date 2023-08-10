02d51ab65d08adbc898d1ec5c5ad81799104cda1
jacobi petrucciani <j@cobi.dev>
2023-08-10 08:32:32 -0400

    llama-cpp: master-ea04a4c -> master-1638757
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-ea04a4c...master-1638757

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -39,7 +39,7 @@ let
       ocl-icd
       opencl-headers
     ];
-  version = "master-ea04a4c";
+  version = "master-1638757";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -49,7 +49,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-DQvh1OE1PNRXs6PiHztibaUCK0jpDrYj997SLIOA1/c=";
+    hash = "sha256-Ku1nejoEgOk/S9udDfJJO8l5JFXIP964T+Vxb1n3QWw=";
   };
 
   postPatch =
