dc339543ece998938f2922a911011dc4fa81ea6e
jacobi petrucciani <j@cobi.dev>
2023-08-21 21:43:26 -0400

    llama-cpp: master-cb1c072 -> master-226255b
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/master-cb1c072...master-226255b

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -39,7 +39,7 @@ let
       ocl-icd
       opencl-headers
     ];
-  version = "master-cb1c072";
+  version = "master-226255b";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -49,7 +49,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-AvXDmX+8H0sfNinI0fOSA+uM1Gl+CE0jdVP889u4o3Y=";
+    hash = "sha256-QAf/bLp/INr/58RcU6H96IgbILuvviLvyXhSxx+TnPw=";
   };
 
   postPatch =
