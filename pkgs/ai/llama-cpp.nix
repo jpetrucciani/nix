aed4411153762269931286c64debcec6292b1972
jacobi petrucciani <j@cobi.dev>
2023-08-30 20:17:31 -0400

    update llama-cpp

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -59,6 +59,9 @@ clangStdenv.mkDerivation rec {
 
   cmakeFlags = [
     "-DLLAMA_BUILD_SERVER=ON"
+    "-DLLAMA_MPI=ON"
+    "-DBUILD_SHARED_LIBS=ON"
+    "-DCMAKE_SKIP_BUILD_RPATH=ON"
   ] ++ (optionals isM1 [
     "-DCMAKE_C_FLAGS=-D__ARM_FEATURE_DOTPROD=1"
     "-DLLAMA_METAL=ON"
