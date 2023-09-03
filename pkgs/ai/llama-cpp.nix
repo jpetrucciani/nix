d9920910f9cdeceea266e552f86850975149aa58
jacobi petrucciani <j@cobi.dev>
2023-09-03 17:20:37 -0400

    llama-cpp: b1170 -> b1171
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/b1170...b1171

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -39,7 +39,7 @@ let
       ocl-icd
       opencl-headers
     ];
-  version = "b1170";
+  version = "b1171";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -49,7 +49,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-KMGbhVvgMsucBHwEOb6MEbXRQFV6M4d4INMi4ABxROk=";
+    hash = "sha256-zINcEujqNG6Y/ta2USGqKeGCMtuNrRcXQKpNqmeCYJU=";
   };
 
   postPatch =
