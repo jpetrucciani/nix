4f8fe0cca642dddfb8c98f878f6fd79ce0f0f8c8
jacobi petrucciani <j@cobi.dev>
2023-09-21 12:24:40 -0400

    python311Packages.llama-index: 0.8.29 -> 0.8.30
    Diff: https://github.com/jerryjliu/llama_index/compare/refs/tags/v0.8.29...v0.8.30

--- mods/python/ai/prompts.nix
+++ mods/python/ai/prompts.nix
@@ -136,14 +136,14 @@ rec {
 
   llama-index = buildPythonPackage rec {
     pname = "llama-index";
-    version = "0.8.29";
+    version = "0.8.30";
     format = "setuptools";
 
     src = fetchFromGitHub {
       owner = "jerryjliu";
       repo = "llama_index";
       rev = "refs/tags/v${version}";
-      hash = "sha256-IZjY8pM4/cJ3yknpaRiHXe3Lmx54uBJOGHbLqYRffJg=";
+      hash = "sha256-+lbvd+OJDxJ1uOtKOU90rJjlnNlTKlVBTYPnOx8Manc=";
     };
 
     nativeBuildInputs = with prev; [
