8c3cd434fa5d8426cf86682a9e4ab828ca9db44a
jacobi petrucciani <j@cobi.dev>
2023-09-24 12:46:18 -0400

    upgrade gitlab-runner

--- mods/pog/hex/k8s/gitlab-runner.nix
+++ mods/pog/hex/k8s/gitlab-runner.nix
@@ -4,13 +4,13 @@ let
     defaults = {
       name = "gitlab-runner";
       namespace = "gitlab";
-      version = "0.56.0";
-      sha256 = "061i3cmklm0d4v6h92mci0i072ak7f31zb86i6sg92gflwadvpgj";
+      version = "0.56.1";
+      sha256 = "1pvyckvx9345z5lawgnaaa4w6dj6j65f2l1m6dycvb22sbkghcqc";
     };
     version = rec {
       _v = v: s: args: chart (args // { version = v; sha256 = s; });
-      latest = v0-54-0;
-      v0-56-0 = _v "0.56.0" "061i3cmklm0d4v6h92mci0i072ak7f31zb86i6sg92gflwadvpgj";
+      latest = v0-56-1;
+      v0-56-1 = _v "0.56.1" "1pvyckvx9345z5lawgnaaa4w6dj6j65f2l1m6dycvb22sbkghcqc";
       v0-55-0 = _v "0.55.0" "18nfn57b6hwd3px43qjpnhf4hz9a13266z32wpi7vv3drmhfvmb0";
       v0-54-0 = _v "0.54.0" "0pmikfklgw68ryngkyjj2swcr13fjjq6iaxf4gvq5pvl9w13q9dm";
       v0-53-2 = _v "0.53.2" "0nr2ikp5xgrd5hi4vhsfbfm91lx66yr61brh4rijk4v6ci4lqk4w";
