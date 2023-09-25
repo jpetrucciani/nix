a105f8335c0658ee812fd09652094ff216b5626d
jacobi petrucciani <j@cobi.dev>
2023-09-25 18:42:54 -0400

    rare: 0.3.1 -> 0.3.3
    Diff: https://github.com/zix99/rare/compare/0.3.1...0.3.3

--- pkgs/cli/rare.nix
+++ pkgs/cli/rare.nix
@@ -1,13 +1,13 @@
 { lib, buildGo120Module, fetchFromGitHub }:
 buildGo120Module rec {
   pname = "rare";
-  version = "0.3.1";
+  version = "0.3.3";
 
   src = fetchFromGitHub {
     owner = "zix99";
     repo = "rare";
     rev = version;
-    sha256 = "sha256-p/L9OL5Eo98PcT5vvODy2xdSH7fuIZJQIAfqhdO490Q=";
+    sha256 = "sha256-83iHYWMdLOzWDu/WW2TN8D2gUe2Y74aGBUjfHIa9ki8=";
   };
 
   vendorHash = "sha256-wUOtxNjL/4MosACCzPTWKWrnMZhxINfN1ppkRsqDh9M=";
