fb03370e58171cf3d1670ca9975d1ae870e22ac8
jacobi petrucciani <j@cobi.dev>
2023-10-15 22:09:04 -0400

    add postgres with pgvector to titan

--- hosts/titan/configuration.nix
+++ hosts/titan/configuration.nix
@@ -83,6 +83,18 @@ in
         };
       };
     };
+    postgresql = {
+      enable = true;
+      package = pkgs.postgresql_16;
+      extraPlugins = with pkgs.postgresql16Packages; [ pgvector ];
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        local all all trust
+        host all all 127.0.0.1/32 trust
+        host all all ::1/128 trust
+        host all all 100.64.0.0/10 trust
+      '';
+    };
   } // common.services;
 
   system.stateVersion = "23.11";
