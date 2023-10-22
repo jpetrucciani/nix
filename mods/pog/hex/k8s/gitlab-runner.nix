58e37291a7a5c1118a1d0132405b740c4b579b90
jacobi petrucciani <j@cobi.dev>
2023-10-22 15:03:18 -0400

    update gitlab-runner helm charts

--- mods/pog/hex/k8s/gitlab-runner.nix
+++ mods/pog/hex/k8s/gitlab-runner.nix
@@ -4,12 +4,14 @@ let
     defaults = {
       name = "gitlab-runner";
       namespace = "gitlab";
-      version = "0.56.1";
-      sha256 = "1pvyckvx9345z5lawgnaaa4w6dj6j65f2l1m6dycvb22sbkghcqc";
+      version = "0.58.0";
+      sha256 = "1hqk44qvv597a9slhlsqpxgq4f8fhgkx6cl5q8102jmdg3qyhhw9";
     };
     version = rec {
       _v = v: s: args: chart (args // { version = v; sha256 = s; });
-      latest = v0-56-1;
+      latest = v0-58-0;
+      v0-58-0 = _v "0.58.0" "1hqk44qvv597a9slhlsqpxgq4f8fhgkx6cl5q8102jmdg3qyhhw9";
+      v0-57-2 = _v "0.57.2" "0wy2l0cpqj9nmzfk6gy5yw55dgkc03lq5mwzkw8g0f1yz43p4f3g";
       v0-56-1 = _v "0.56.1" "1pvyckvx9345z5lawgnaaa4w6dj6j65f2l1m6dycvb22sbkghcqc";
       v0-55-0 = _v "0.55.0" "18nfn57b6hwd3px43qjpnhf4hz9a13266z32wpi7vv3drmhfvmb0";
       v0-54-0 = _v "0.54.0" "0pmikfklgw68ryngkyjj2swcr13fjjq6iaxf4gvq5pvl9w13q9dm";
