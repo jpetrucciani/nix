35395b52e81326654d67bba637cc5bb788783a27
jacobi petrucciani <j@cobi.dev>
2023-10-24 17:59:10 -0400

    llama-cpp: b1422 -> b1426
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/b1422...b1426

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -39,7 +39,7 @@ let
       ocl-icd
       opencl-headers
     ];
-  version = "b1422";
+  version = "b1426";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -49,7 +49,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-eBy9Yo0VE3dS1gAUYQwX9rgkViHUehz0tm1dVeDLZf8=";
+    hash = "sha256-EhVqb6L1xB8pfpajpXfwIUk5aOEJem2XAD2+YwWoAtY=";
   };
 
   postPatch =
