098d0be8716d27d5c4af7b540014412d63c86bd1
jacobi petrucciani <j@cobi.dev>
2023-11-02 21:21:31 -0400

    update tailscale -> 1.52.0

--- mods/pog/hex/k8s/tailscale.nix
+++ mods/pog/hex/k8s/tailscale.nix
@@ -17,9 +17,9 @@ let
         memory = "2Gi";
       };
       tailscale_image_base = "ghcr.io/tailscale/tailscale";
-      tailscale_image_tag = "v1.50.1";
+      tailscale_image_tag = "v1.52.0";
       cloudsql_image_base = "gcr.io/cloudsql-docker/gce-proxy";
-      cloudsql_image_tag = "1.33.11";
+      cloudsql_image_tag = "1.33.13";
 
       tags = [ "k8s" "proxy" ];
       cidr = "100.64.0.0/10";
