8fca64d3120f5563fa62816c03b14e6c70e9c8e7
jacobi petrucciani <j@cobi.dev>
2023-11-05 09:55:30 -0500

    python311Packages.reflex: 0.3.1 -> 0.3.2
    Diff: https://github.com/reflex-dev/reflex/compare/refs/tags/v0.3.1...v0.3.2

--- mods/python/experimental.nix
+++ mods/python/experimental.nix
@@ -7,7 +7,7 @@ final: prev: with prev; rec {
     in
     buildPythonPackage rec {
       pname = "reflex";
-      version = "0.3.1";
+      version = "0.3.2";
       format = "pyproject";
 
 
@@ -15,7 +15,7 @@ final: prev: with prev; rec {
         owner = "reflex-dev";
         repo = pname;
         rev = "refs/tags/v${version}";
-        sha256 = "sha256-dW8bRo4isDpI5wiStloyoqiw6piLScsmM4jcJPsul0c=";
+        sha256 = "sha256-NSTLgLbTvwMxQJSON5irwIeDtp4WeLSa5X97LpGdecI=";
       };
 
       propagatedBuildInputs = [
@@ -331,7 +331,7 @@ final: prev: with prev; rec {
 
   emmett-crypto = buildPythonPackage rec {
     pname = "emmett-crypto";
-    version = "0.3.1";
+    version = "0.3.2";
 
     format = "pyproject";
     src = pkgs.fetchFromGitHub {
@@ -372,7 +372,7 @@ final: prev: with prev; rec {
 
   granian = buildPythonPackage rec {
     pname = "granian";
-    version = "0.3.1";
+    version = "0.3.2";
 
     format = "pyproject";
     src = pkgs.fetchFromGitHub {
