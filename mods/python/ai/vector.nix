96d14a819fa5b5d5cc0583da15f8ac130ab01194
jacobi petrucciani <j@cobi.dev>
2023-11-08 08:33:40 -0500

    python311Packages.chromadb: 0.4.15 -> 0.4.16

--- mods/python/ai/vector.nix
+++ mods/python/ai/vector.nix
@@ -10,12 +10,12 @@ in
     in
     buildPythonPackage rec {
       pname = "chromadb";
-      version = "0.4.15";
+      version = "0.4.16";
       format = "pyproject";
 
       src = fetchPypi {
         inherit pname version;
-        hash = "sha256-/ShiH04zsWip1InvtAFMmIeBqMoGVZh4HTNrcoIaktY=";
+        hash = "sha256-1fsRPqAvh7lpiHJ5rsYl4aKmi/as7fFgn5XSdnCnjcA=";
       };
 
       nativeBuildInputs = with prev; [
