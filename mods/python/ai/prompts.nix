ee2b0556cac2423e4811b5ffcab65e7759976cab
jacobi petrucciani <j@cobi.dev>
2023-11-10 08:48:44 -0500

    python311Packages.lmql: init at 0.7.3

--- mods/python/ai/prompts.nix
+++ mods/python/ai/prompts.nix
@@ -433,4 +433,54 @@ rec {
         maintainers = with maintainers; [ jpetrucciani ];
       };
     };
+
+  lmql = buildPythonPackage rec {
+    pname = "lmql";
+    version = "0.7.3";
+    pyproject = true;
+
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-PAxGk0NYOIY9WKNlJP9wmVBotn7fLp2kqtiehkFphnU=";
+    };
+
+    nativeBuildInputs = with prev; [
+      setuptools
+      wheel
+    ];
+
+    propagatedBuildInputs = with prev; [
+      aiohttp
+      astunparse
+      llama-cpp-python
+      numpy
+      openai
+      termcolor
+      tiktoken
+    ];
+
+    passthru.optional-dependencies = with prev; {
+      hf = [
+        accelerate
+        transformers
+      ];
+      replicate = [
+        aiohttp-sse-client
+        transformers
+      ];
+      tests = [
+        pytest
+        pytest-asyncio
+      ];
+    };
+
+    pythonImportsCheck = [ "lmql" ];
+
+    meta = {
+      description = "A query language for language models";
+      homepage = "https://lmql.ai";
+      license = licenses.asl20;
+      maintainers = with maintainers; [ jpetrucciani ];
+    };
+  };
 }
