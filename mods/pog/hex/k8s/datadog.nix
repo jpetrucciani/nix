82b6837ad4ee328fcc720557a24b4d8b12035d13
jacobi petrucciani <j@cobi.dev>
2023-11-14 19:06:05 -0500

    update latest datadog chart

--- mods/pog/hex/k8s/datadog.nix
+++ mods/pog/hex/k8s/datadog.nix
@@ -4,14 +4,15 @@ let
     defaults = {
       name = "datadog";
       namespace = "default";
-      version = "3.1.9";
-      sha256 = "1wryym5v8pr70r3fs4i4y2mq8dihla07djnyis0q68l1j3m27mzs";
+      version = "3.45.0";
+      sha256 = "0z306k1v4zna4jp96yr9sh3aq9hqx5riapxymd3w8zq440n0w2h8";
     };
     version = rec {
       _v = v: s: args: chart (args // { version = v; sha256 = s; });
+      latest = v3-45-0;
+      v3-45-0 = _v defaults.version defaults.sha256;
+      v3-1-9 = _v "3.1.9" "1wryym5v8pr70r3fs4i4y2mq8dihla07djnyis0q68l1j3m27mzs";
       v3-1-3 = _v "3.1.3" "004mr6jj046dqwfbd4zrs6qj8wqh9l8hwvrym9akdqr5fkilizzb";
-      v3-1-9 = _v defaults.version defaults.sha256;
-      latest = v3-1-9;
     };
     chart_url = version: hex.k8s.helm.charts.url.github {
       inherit version;
