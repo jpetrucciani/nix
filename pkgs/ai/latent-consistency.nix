205d0902b6446a841a70601b78ea832c63aa3b22
jacobi petrucciani <j@cobi.dev>
2023-11-20 10:26:37 -0500

    fix lcm

--- pkgs/ai/latent-consistency.nix
+++ pkgs/ai/latent-consistency.nix
@@ -14,11 +14,7 @@ let
               self.pipe = self._load_model()
 
           def _load_model(self):
-              model = DiffusionPipeline.from_pretrained(
-                  "SimianLuo/LCM_Dreamshaper_v7",
-                  custom_pipeline="latent_consistency_txt2img",
-                  custom_revision="main"
-              )
+              model = DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7")
               model.to(torch_device="cpu", torch_dtype=torch.float32).to('mps:0')
               return model
 
