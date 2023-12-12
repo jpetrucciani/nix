fd511c828d0e8727c73a08979de18200d9c58e47
jacobi petrucciani <j@cobi.dev>
2023-12-12 14:17:13 -0500

    update otf chart -> 0.3.9

--- mods/pog/hex/k8s/otf.nix
+++ mods/pog/hex/k8s/otf.nix
@@ -5,13 +5,14 @@ let
     defaults = {
       inherit name;
       namespace = name;
-      version = "0.3.6";
-      sha256 = "0yamqqs88zrbpp89hy3ra9w5c40ply9rd0b1l9icclhmq5nr3kgl";
+      version = "0.3.9";
+      sha256 = "1d5vjrgx5m9li8iw0i0qyrxdw0x120nqvhmvq2871r2xizmf6pa0";
     };
     version = rec {
       _v = v: s: args: chart (args // { version = v; sha256 = s; });
-      latest = v0-3-6;
-      v0-3-6 = _v defaults.version defaults.sha256;
+      latest = v0-3-9;
+      v0-3-9 = _v defaults.version defaults.sha256;
+      v0-3-6 = _v "0.3.6" "0yamqqs88zrbpp89hy3ra9w5c40ply9rd0b1l9icclhmq5nr3kgl";
     };
     chart_url = version: "https://github.com/leg100/otf-charts/releases/download/${name}-${version}/${name}-${version}.tgz";
     chart =
