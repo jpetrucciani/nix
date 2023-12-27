83ad2cb40740ef9ca644ef1468cfddddcde3dcaa
jacobi petrucciani <j@cobi.dev>
2023-12-27 11:29:26 -0500

    rework a bit

--- mods/override.nix
+++ mods/override.nix
@@ -1,8 +1,15 @@
 final: prev:
 let
   inherit (final.stdenv) isDarwin;
+  zitadel_pr = import
+    (builtins.fetchTarball {
+      url = "https://github.com/jpetrucciani/nixpkgs/archive/535a908c603f65a6ceed636221a3148465cfb716.tar.gz";
+      sha256 = "13jgyc0b9l6j80dzsk1bwia4vsyh7q7kwxjl5fv402gypycij275";
+    })
+    { system = "x86_64-linux"; };
 in
 {
+  inherit (zitadel_pr) zitadel;
   # fix for getting yank working on darwin
   yank = prev.yank.overrideAttrs (attrs: {
     makeFlags = if isDarwin then [ "YANKCMD=/usr/bin/pbcopy" ] else attrs.makeFlags;
@@ -21,11 +28,4 @@ in
     });
     nodeRuntimes = [ "node16" "node20" ];
   };
-
-  zitadel = (import
-    (builtins.fetchTarball {
-      url = "https://github.com/jpetrucciani/nixpkgs/archive/535a908c603f65a6ceed636221a3148465cfb716.tar.gz";
-      sha256 = "13jgyc0b9l6j80dzsk1bwia4vsyh7q7kwxjl5fv402gypycij275";
-    })
-    { system = "x86_64-linux"; }).zitadel;
 }
