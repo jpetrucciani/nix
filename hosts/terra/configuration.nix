93346169f39cb5c499a56c203218ba5e90daad53
jacobi petrucciani <j@cobi.dev>
2023-12-27 12:17:48 -0500

    more edits to zitadel config

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -320,6 +320,14 @@ in
       settings = {
         ExternalDomain = "z.cobi.dev";
         ExternalPort = 443;
+        DefaultInstance = {
+          LoginPolicy = {
+            AllowRegister = false;
+          };
+          Restrictions = {
+            DisallowPublicOrgRegistration = true;
+          };
+        };
       };
       extraSettingsPaths = [ config.age.secrets.zitadel.path ];
     };
