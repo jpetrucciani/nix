ef2bdaad5704810d5a8dacbb5725dc97c62b41f9
jacobi petrucciani <j@cobi.dev>
2023-12-28 00:01:00 -0500

    fix firewall issue with k3s on terra

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -55,7 +55,7 @@ in
     firewall = {
       enable = true;
       trustedInterfaces = [ "tailscale0" ];
-      allowedTCPPorts = with common.ports; usual;
+      allowedTCPPorts = with common.ports; [ 6443 ] ++ usual;
       allowedUDPPorts = [ ];
       checkReversePath = "loose";
     };
@@ -325,6 +325,16 @@ in
       extraSettingsPaths = [ config.age.secrets.zitadel.path ];
     };
   } // common.services;
+
+  # https://github.com/NixOS/nixpkgs/issues/103158
+  systemd.services.k3s.after = [ "network-online.service" "firewall.service" ];
+  systemd.services.k3s.serviceConfig.KillMode = pkgs.lib.mkForce "control-group";
+
+  # https://github.com/NixOS/nixpkgs/issues/98766
+  boot.kernelModules = [ "br_netfilter" "ip_conntrack" "ip_vs" "ip_vs_rr" "ip_vs_wrr" "ip_vs_sh" "overlay" ];
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -i cni+ -j ACCEPT
+  '';
 
   systemd.services.lemmy.serviceConfig.EnvironmentFile = "/etc/default/lemmy";
   systemd.services.lemmy.serviceConfig.ExecStartPre =
