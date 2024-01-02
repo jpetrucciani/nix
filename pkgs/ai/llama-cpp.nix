e1953d57845f206d154c002d94b62380aa6842fb
jacobi petrucciani <j@cobi.dev>
2024-01-02 11:09:07 -0500

    llama-cpp: b1748 -> b1750
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/b1748...b1750

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -50,7 +50,7 @@ let
       "${cudatoolkit}/targets/${system}"
     ];
   };
-  version = "b1748";
+  version = "b1750";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -60,7 +60,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-x+ruCIdPRuJCoF7AatUcgTkSseQszoQPE64ueLt5Mzk=";
+    hash = "sha256-ydDeTRWydIuMEiXU/1zjcJLwjqAxyRqVt1CDV7moe/w=";
   };
 
   postPatch =
