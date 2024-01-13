4a44510399548ef667ee1089c009860bc26f0962
jacobi petrucciani <j@cobi.dev>
2024-01-13 17:16:06 -0500

    llama-cpp: b1860 -> b1861
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/b1860...b1861

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -50,7 +50,7 @@ let
       "${cudatoolkit}/targets/${system}"
     ];
   };
-  version = "b1860";
+  version = "b1861";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -60,7 +60,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-NPzTBIxeqNM2xF40Syz6aFPrJdEjyDtu1QneiO+sllo=";
+    hash = "sha256-xDPYRtDL1McwjH15oI68wJjTT8yR+yunOA7rkY1EdaY=";
   };
 
   postPatch =
