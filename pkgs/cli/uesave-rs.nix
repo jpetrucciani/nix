4332286d3ca5b5ca63fb0b2ef774f95a88bf15ca
jacobi petrucciani <j@cobi.dev>
2024-01-23 12:32:28 -0500

    uesave-rs: init at 0.3.0

---
+++ pkgs/cli/uesave-rs.nix
@@ -0,0 +1,26 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "uesave-rs";
+  version = "0.3.0";
+
+  src = fetchFromGitHub {
+    owner = "trumank";
+    repo = "uesave-rs";
+    rev = "v${version}";
+    hash = "sha256-YRn14rF94zSTnFAIRuvw84GDRBaqmns9nvaHCTjhWQg=";
+  };
+
+  cargoHash = "sha256-MO4LKQbIwiYJzFTMglx4uet8UYtPE+H3MfSEHm0JmOI=";
+
+  meta = with lib; {
+    description = "Rust library to read and write Unreal Engine save files";
+    homepage = "https://github.com/trumank/uesave-rs";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+    mainProgram = "uesave-rs";
+  };
+}
