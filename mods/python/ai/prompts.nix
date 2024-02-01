7ead0860b8d9f308a79b6f88e7be1d4e939eb534
jacobi petrucciani <j@cobi.dev>
2024-02-01 13:10:06 -0500

    python311Packages.langchain-core: 0.1.16 -> 0.1.18

--- mods/python/ai/prompts.nix
+++ mods/python/ai/prompts.nix
@@ -73,13 +73,13 @@ rec {
 
   langchain-core = buildPythonPackage rec {
     pname = "langchain-core";
-    version = "0.1.16";
+    version = "0.1.18";
     pyproject = true;
 
     src = fetchPypi {
       pname = "langchain_core";
       inherit version;
-      hash = "sha256-jLVG7tMYAJ7hqKOB0QgHTt3wOVrmHrJD2wDXbh4mXok=";
+      hash = "sha256-rUcLIc39x16CnNkcjY634EOKuN21tQgoEl/3raEh7ns=";
     };
 
     nativeBuildInputs = with final; [
