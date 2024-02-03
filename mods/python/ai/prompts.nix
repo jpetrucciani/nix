9699c01751557cb6b9183c7e0a9baa536576e55f
jacobi petrucciani <j@cobi.dev>
2024-02-03 17:07:38 -0500

    python311Packages.llama-index: 0.9.42 -> 0.9.43
    Diff: https://github.com/run-llama/llama_index/compare/refs/tags/v0.9.42...v0.9.43

--- mods/python/ai/prompts.nix
+++ mods/python/ai/prompts.nix
@@ -239,14 +239,14 @@ rec {
 
   llama-index = buildPythonPackage rec {
     pname = "llama-index";
-    version = "0.9.42";
+    version = "0.9.43";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "run-llama";
       repo = "llama_index";
       rev = "refs/tags/v${version}";
-      hash = "sha256-uZESxBDsMzo++r2SAUabWU0S1SsXMtfESrhbPfAFNC4=";
+      hash = "sha256-Q8NtpVBs6Zl5auduCI3W61Iv7OC50Cnxyq9U9Ek0Zr0=";
     };
 
     nativeBuildInputs = with prev; [
