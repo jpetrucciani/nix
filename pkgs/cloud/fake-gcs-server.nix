6af57c3bc2a9d4b05a5cbab3962c825d2396717f
jacobi petrucciani <j@cobi.dev>
2024-02-07 08:51:20 -0500

    fake-gcs-server: 1.47.7 -> 1.47.8
    Diff: https://github.com/fsouza/fake-gcs-server/compare/v1.47.7...v1.47.8

--- pkgs/cloud/fake-gcs-server.nix
+++ pkgs/cloud/fake-gcs-server.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "fake-gcs-server";
-  version = "1.47.7";
+  version = "1.47.8";
 
   src = fetchFromGitHub {
     owner = "fsouza";
     repo = "fake-gcs-server";
     rev = "v${version}";
-    hash = "sha256-TM6lJLlxcdZjcW1MyXzlqXIntEAZgiy3fqxrxP5VduQ=";
+    hash = "sha256-7xvqxBTYGlO8cBQcNVnrH6TGUllLq9Lgsh4fYn5LCnE=";
   };
 
   doCheck = false;
@@ -23,7 +23,7 @@ buildGoModule rec {
       internal/backend/backend_test.go
   '';
 
-  vendorHash = "sha256-DhOaZ2iFhedpc/j1RA8jthTGPRNR5LZQvVc+CAO3V7M=";
+  vendorHash = "sha256-n1iu84hQLHzPkb/uQ8yeNIJxtu+65omWIZwJ4Mwirvg=";
 
   ldflags = [
     "-s"
