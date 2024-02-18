350eda5c1868afb27902caf138ad73a2efbf47cc
jacobi petrucciani <j@cobi.dev>
2024-02-18 17:26:19 -0500

    chart updates

--- mods/pog/hex/k8s/flipt.nix
+++ mods/pog/hex/k8s/flipt.nix
@@ -5,13 +5,15 @@ let
     defaults = {
       inherit name;
       namespace = name;
-      version = "0.52.0";
-      sha256 = "1p1hzwz181x7dvyln98qdjfswkrnkk122m7z0vm1sd7ycazbv9wz";
+      version = "0.53.1";
+      sha256 = "1wgmnbms95rnjd3dq09dwiiwdnrq95pc9n0v6763b5j9s7yf7alc";
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v0-52-0;
-      v0-52-0 = _v defaults.version defaults.sha256; # 2024-01-23
+      latest = v0-53-1;
+      v0-53-1 = _v "0.53.1" "1wgmnbms95rnjd3dq09dwiiwdnrq95pc9n0v6763b5j9s7yf7alc"; # 2024-02-12
+      v0-53-0 = _v "0.53.0" "1fdq507fjinancnfgy0qkfx91z7p9yp84qxxld4khr0fh791bfv2"; # 2024-02-09
+      v0-52-0 = _v "0.52.0" "1p1hzwz181x7dvyln98qdjfswkrnkk122m7z0vm1sd7ycazbv9wz"; # 2024-01-23
       v0-51-0 = _v "0.51.0" "0vnb965a10s8qzjgr11ph40g6ji1cnm6j93rs0qshnn4jkg13553";
     };
     chart_url = version: "https://github.com/flipt-io/helm-charts/releases/download/${name}-${version}/${name}-${version}.tgz";
--- mods/pog/hex/k8s/gitlab-runner.nix
+++ mods/pog/hex/k8s/gitlab-runner.nix
@@ -4,14 +4,18 @@ let
     defaults = {
       name = "gitlab-runner";
       namespace = "gitlab";
-      version = "0.60.0";
-      sha256 = "1k0gdg2mvm98s8msrsnr0aqsblhigbr7y6fg263yd6mmg8yzxlcp";
+      version = "0.62.0";
+      sha256 = "1h8zj211a6z1fmrs8ikf232fswyrf4x8zk90kdq84yga62brvbvr";
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v0-59-3;
-      v0-60-0 = _v "0.60.0" "1k0gdg2mvm98s8msrsnr0aqsblhigbr7y6fg263yd6mmg8yzxlcp";
-      v0-59-3 = _v "0.59.3" "11i6p6bp2ysxyv6v22lv7z8d6nc8nlibvbvvrqxnivrp7wjl4yah";
+      latest = v0-62-0;
+      v0-62-0 = _v "0.62.0" "1h8zj211a6z1fmrs8ikf232fswyrf4x8zk90kdq84yga62brvbvr"; # 2024-02-15
+      v0-61-3 = _v "0.61.3" "1qvv5zazaa7039jjkbh4rbi36dnx0s6zxvs73b211bz3jb6wrj5v"; # 2024-02-15
+      v0-61-0 = _v "0.61.0" "0hc4b1v350hlnr0filwccza6bfyx0grabq8ldfgnc6chbl4m323d"; # 2024-01-19
+      v0-60-1 = _v "0.60.1" "0r224arp4zi76d7sbybg9rdnwzwkl6ayq8iwb3v2zk8w83p0y33y"; # 2024-01-19
+      v0-60-0 = _v "0.60.0" "1k0gdg2mvm98s8msrsnr0aqsblhigbr7y6fg263yd6mmg8yzxlcp"; # 2023-12-21
+      v0-59-3 = _v "0.59.3" "11i6p6bp2ysxyv6v22lv7z8d6nc8nlibvbvvrqxnivrp7wjl4yah"; # 2023-12-21
       v0-58-2 = _v "0.58.2" "0h9nzls8g9gbh371y2z41lpc4v2yyd515dccqbrczd2dlziqzsm8";
       v0-57-2 = _v "0.57.2" "0wy2l0cpqj9nmzfk6gy5yw55dgkc03lq5mwzkw8g0f1yz43p4f3g";
       v0-56-1 = _v "0.56.1" "1pvyckvx9345z5lawgnaaa4w6dj6j65f2l1m6dycvb22sbkghcqc";
