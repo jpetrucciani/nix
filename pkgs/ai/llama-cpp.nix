9fb22bbe7b6d71e3dc88c9293e34a51d53f091ac
jacobi petrucciani <j@cobi.dev>
2024-02-18 22:19:50 -0500

    llama-cpp: b2177 -> b2185
    Diff: https://github.com/ggerganov/llama.cpp/compare/refs/tags/b2177...b2185

--- pkgs/ai/llama-cpp.nix
+++ pkgs/ai/llama-cpp.nix
@@ -50,7 +50,7 @@ let
       "${cudatoolkit}/targets/${system}"
     ];
   };
-  version = "b2177";
+  version = "b2185";
   owner = "ggerganov";
   repo = "llama.cpp";
 in
@@ -60,7 +60,7 @@ clangStdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-4M8r1xwYVBx3LwTTKcZvcx694fTr/4IQZKaGQWzKeSM=";
+    hash = "sha256-9yjnKJ4hsy/7go975GgDZuXkBpJQTjsiGaOPn6+na9s=";
   };
 
   postPatch =
