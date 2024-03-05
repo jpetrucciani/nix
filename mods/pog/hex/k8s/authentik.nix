e56f6d4ae5c16f2845103a0f53842c9afa0b359c
jacobi petrucciani <j@cobi.dev>
2024-03-05 11:36:28 -0500

    update authentik charts

--- mods/pog/hex/k8s/authentik.nix
+++ mods/pog/hex/k8s/authentik.nix
@@ -4,13 +4,16 @@ let
     defaults = {
       name = "authentik";
       namespace = "default";
-      version = "2023.10.7";
-      sha256 = "1akcij9kkplsx7jhlvbckcx0cvsqizr6ahglxw535r7f519cmnhd";
+      version = "2024.2.2";
+      sha256 = "0dqs43sxva6n3xnsgmqs28854wl4jy0lcw8czajpch2mzdqsdwhc";
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v2023-10-7;
-      v2023-10-7 = _v defaults.version defaults.sha256; # 2024-01-29
+      latest = v2024-2-2;
+      v2024-2-2 = _v "2024.2.2" "0dqs43sxva6n3xnsgmqs28854wl4jy0lcw8czajpch2mzdqsdwhc"; # 2024-03-04
+      v2024-2-1 = _v "2024.2.1" "17fgvarciwnrp02s420bgbizqb0lyglvzqgkjl3i6gnnazgc395a"; # 2024-02-22
+      v2024-2-0 = _v "2024.2.0" "0dssij0lcr3qlql10aydwd8z4s3j5lw0ykdlrcfr3xq58p59hm2g"; # 2024-02-21
+      v2023-10-7 = _v "2023.10.7" "1akcij9kkplsx7jhlvbckcx0cvsqizr6ahglxw535r7f519cmnhd"; # 2024-01-29
       v2023-10-6 = _v "2023.10.6" "1pfjbz5ll3fgqw4qh4axri8fb11kjcf771zi09lxj2jxki6c9m36";
       v2023-8-3 = _v "2023.8.3" "1n0pqzmnypls3s9gggnsnyapi2b96isyd8p3x0cvzrysx5ah9ql1";
       v2023-6-3 = _v "2023.6.3" "0wz502fd327wqq3pdamm19gkz9isrj0ldqya5ksbmbjdhcb8jzwp";
