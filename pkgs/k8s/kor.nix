46b3b3aa635b105b7b3f7f579272645a52f5c4b8
jacobi petrucciani <j@cobi.dev>
2024-03-19 09:23:55 -0400

    kor: init at 0.3.6

---
+++ pkgs/k8s/kor.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
+
+buildGoModule rec {
+  pname = "kor";
+  version = "0.3.6";
+
+  src = fetchFromGitHub {
+    owner = "yonahd";
+    repo = "kor";
+    rev = "v${version}";
+    hash = "sha256-Q2VUc91ecBRr/m9DGYWwuSsH2prB+EKmBoQrekgPvTE=";
+  };
+
+  vendorHash = "sha256-DRbwM6fKTIlefD0rUmNLlUXrK+t3vNCl4rxHF7m8W10=";
+
+  doCheck = false;
+  CGO_ENABLED = 0;
+  ldflags = [
+    "-X=github.com/yonahd/kor/pkg/utils.Version=${version}"
+  ];
+
+  meta = with lib; {
+    description = "A Golang Tool to discover unused Kubernetes Resources";
+    homepage = "https://github.com/yonahd/kor";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jpetrucciani ];
+    mainProgram = "kor";
+  };
+}
