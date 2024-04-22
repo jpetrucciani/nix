94879c8ab2a84f412b18313d2d03861de54fb5ac
jacobi petrucciani <j@cobi.dev>
2024-04-22 15:00:43 -0400

    koboldcpp: 1.62.1 -> 1.63
    Diff: https://github.com/LostRuins/koboldcpp/compare/refs/tags/v1.62.1...v1.63

--- pkgs/ai/koboldcpp.nix
+++ pkgs/ai/koboldcpp.nix
@@ -29,7 +29,7 @@ let
       ocl-icd
       opencl-headers
     ];
-  version = "1.62.1";
+  version = "1.63";
   owner = "LostRuins";
   repo = "koboldcpp";
   py = python311.withPackages (p: with p; [
@@ -44,7 +44,7 @@ llvmPackages_14.stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/v${version}";
-    hash = "sha256-aKPXvUDG14INASkHFdNv1FkRKWNl6qsxVRkJKqgkDxY=";
+    hash = "sha256-LAAINN49gjJPye3rfbA8oh29/qcVVk99SSRZD8WQNKQ=";
   };
 
   postPatch = optionals isM1 ''
