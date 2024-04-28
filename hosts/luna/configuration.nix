854b94199fe8e24f576be62a6106399ed0ab12f6
jacobi petrucciani <j@cobi.dev>
2024-04-28 11:03:35 -0400

    more loki updates

--- hosts/luna/configuration.nix
+++ hosts/luna/configuration.nix
@@ -325,7 +325,7 @@ in
             from = "2022-06-06";
             store = "boltdb-shipper";
             object_store = "filesystem";
-            schema = "v11";
+            schema = "v13";
             index = {
               prefix = "index_";
               period = "24h";
