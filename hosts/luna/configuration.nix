eb588716ad1e15e816f7dcdcf3ce29c65835f3e5
jacobi petrucciani <j@cobi.dev>
2024-04-28 11:02:22 -0400

    fix loki config options?

--- hosts/luna/configuration.nix
+++ hosts/luna/configuration.nix
@@ -304,7 +304,6 @@ in
       configuration = {
         server.http_listen_port = common.ports.loki;
         auth_enabled = false;
-
         ingester = {
           lifecycler = {
             address = "127.0.0.1";
@@ -319,7 +318,6 @@ in
           max_chunk_age = "1h";
           chunk_target_size = 999999;
           chunk_retain_period = "30s";
-          max_transfer_retries = 0;
         };
 
         schema_config = {
@@ -340,7 +338,6 @@ in
             active_index_directory = "/var/lib/loki/boltdb-shipper-active";
             cache_location = "/var/lib/loki/boltdb-shipper-cache";
             cache_ttl = "24h";
-            shared_store = "filesystem";
           };
 
           filesystem = {
@@ -353,10 +350,6 @@ in
           reject_old_samples_max_age = "168h";
         };
 
-        chunk_store_config = {
-          max_look_back_period = "0s";
-        };
-
         table_manager = {
           retention_deletes_enabled = false;
           retention_period = "0s";
@@ -364,7 +357,6 @@ in
 
         compactor = {
           working_directory = "/var/lib/loki";
-          shared_store = "filesystem";
           compactor_ring = {
             kvstore = {
               store = "inmemory";
