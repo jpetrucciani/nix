904d75f85fe7c9c692bdaf7e53c430126a6ce012
jacobi petrucciani <j@cobi.dev>
2024-04-30 14:39:07 -0400

    fix dulwich problem

--- mods/python/fixes.nix
+++ mods/python/fixes.nix
@@ -85,7 +85,13 @@ rec {
     doCheck = false;
   });
 
-  dulwich = prev.dulwich.overridePythonAttrs (_: {
+  dulwich = prev.dulwich.overridePythonAttrs (_: rec{
+    version = "0.21.7";
+    pname = "dulwich";
+    src = prev.fetchPypi {
+      inherit pname version;
+      hash = "sha256-qenGaDPOpYDDrBKSfkuXEZhddq/KmNqXFAXUFN5g6Wg=";
+    };
     doCheck = false;
   });
 
