e6810d6a3fc34cf11a9e335f96b9a20ef369c4b3
jacobi petrucciani <j@cobi.dev>
2024-05-27 12:32:19 -0400

    whisper-cpp: 1.6.1 -> 1.6.2
    Diff: https://github.com/ggerganov/whisper.cpp/compare/refs/tags/v1.6.1...v1.6.2

--- pkgs/ai/whisper-cpp.nix
+++ pkgs/ai/whisper-cpp.nix
@@ -27,7 +27,7 @@ let
       cudatoolkit.out
     ];
   };
-  version = "1.6.1";
+  version = "1.6.2";
   owner = "ggerganov";
   repo = "whisper.cpp";
 in
@@ -37,7 +37,7 @@ clangStdenv.mkDerivation {
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/v${version}";
-    hash = "sha256-0QVaBV/kOMBHon0L8L+LDrFra2forqy441z7KLPNaa8=";
+    hash = "sha256-hIEIu7feOZWqxRskf6Ej7l653/9KW8B3cnpPLoCRBAc=";
   };
 
   # flags
