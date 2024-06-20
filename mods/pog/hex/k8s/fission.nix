47528513e5fed8b9f9fb9a81b18dface824cf5fb
jacobi petrucciani <j@cobi.dev>
2024-06-19 21:42:40 -0400

    fix fission chart

--- mods/pog/hex/k8s/fission.nix
+++ mods/pog/hex/k8s/fission.nix
@@ -14,7 +14,7 @@ let
       v1-20-1 = _v "1.20.1" "0yrba33qh6dyqgam31lx3jr4b7ah9gfcbc25b32ffivmp6fdwdgs"; # 2024-01-14
       v1-20-0 = _v defaults.version defaults.sha256;
     };
-    chart_url = version: "https://github.com/fission/fission-charts/releases/download/${name}-all-${version}/${name}-all-${version}.tgz";
+    chart_url = version: "https://github.com/fission/fission-charts/releases/download/${name}-all-v${version}/${name}-all-v${version}.tgz";
     chart = hex.k8s._.chart { inherit defaults chart_url; };
   };
 in
