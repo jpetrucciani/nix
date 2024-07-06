72807d082387cab480d5577d9b0c7f7cc3bfe44f
jacobi petrucciani <j@cobi.dev>
2024-07-06 15:11:02 -0400

    update obligator

--- pkgs/server/obligator.nix
+++ pkgs/server/obligator.nix
@@ -6,13 +6,13 @@
 
 buildGoModule {
   pname = "obligator";
-  version = "0.2.0";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "anderspitman";
     repo = "obligator";
-    rev = "37f75cc861f1bcd0dbf0f26a58e0f45bdae032ff";
-    hash = "sha256-Di4nH/veqp1dTwJBavRENOI/NMsvhNtgN0QZ4OU4lfw=";
+    rev = "623276f2ff6a390865cce0627f0ac3c4e584b062";
+    hash = "sha256-zNR27CthwAf9QieYe7Ei/VZ8Bn30Sxi3L0BR4zcqmqM=";
   };
 
   vendorHash = "sha256-snI3htyuBeeTOND8X5Wi+3WEW5xC+Y+C8jMfmiYAur4=";
