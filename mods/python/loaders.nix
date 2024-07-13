09ae7c32296e0d5afbe669d32b6675e19cfc5607
jacobi petrucciani <j@cobi.dev>
2024-07-13 12:01:42 -0400

    python311Packages.selectolax: init at 0.3.21

--- mods/python/loaders.nix
+++ mods/python/loaders.nix
@@ -2,6 +2,7 @@ final: prev:
 let
   inherit (final) buildPythonPackage fetchPypi;
   inherit (final.lib) licenses maintainers;
+  inherit (final.pkgs) fetchFromGitHub;
 in
 {
   docx2txt = buildPythonPackage
@@ -24,4 +25,34 @@ in
         maintainers = with maintainers; [ jpetrucciani ];
       };
     };
+
+  selectolax = buildPythonPackage rec {
+    pname = "selectolax";
+    version = "0.3.21";
+    pyproject = true;
+
+    src = fetchFromGitHub {
+      owner = "rushter";
+      repo = "selectolax";
+      rev = "v${version}";
+      hash = "sha256-Ab09I0CUq/FB4Rtl+ccgr9ap7vCAeujdSNTmCVjEOjs=";
+      fetchSubmodules = true;
+    };
+
+    nativeBuildInputs = with final; [
+      cython
+      setuptools
+      wheel
+    ];
+
+    pythonImportsCheck = [ "selectolax" ];
+
+    meta = {
+      description = "Python binding to Modest and Lexbor engines (fast HTML5 parser with CSS selectors";
+      homepage = "https://github.com/rushter/selectolax";
+      changelog = "https://github.com/rushter/selectolax/blob/${src.rev}/CHANGES.rst";
+      license = licenses.mit;
+      maintainers = with maintainers; [ jpetrucciani ];
+    };
+  };
 }
