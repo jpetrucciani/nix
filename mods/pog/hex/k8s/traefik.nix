7de30346040c18b1d9ce41432b096ebe2b6a2e6a
jacobi petrucciani <j@cobi.dev>
2024-07-24 17:54:32 -0400

    update traefik chart -> 30.0.0

--- mods/pog/hex/k8s/traefik.nix
+++ mods/pog/hex/k8s/traefik.nix
@@ -10,7 +10,8 @@ let
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v29-0-1;
+      latest = v30-0-0;
+      v30-0-0 = _v "30.0.0" "18zvj0641ilhkrymcgjrai9hqlpbq57d2sv597jspw94avsr4xaw"; # 2024-07-24
       v29-0-1 = _v "29.0.1" "1d1nb55jmfaks0pqga9yyy920f3pnk6909hlagjwanqwvzxq2zn4"; # 2024-07-09
       v28-3-0 = _v "28.3.0" "1nahyic3gwcgmlh5c4k0wbd3bdraggkp0c51kc75yq192hfkn6n1"; # 2024-06-14
       v27-0-2 = _v "27.0.2" "1mfbyh1ihknkxb1nmasikz7cy6vqbw8741hqdxmh8p1myvldk59k"; # 2024-04-12
