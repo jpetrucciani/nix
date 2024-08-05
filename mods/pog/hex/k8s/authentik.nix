d2362c5f641e43eba05129ec88e76ac981330fd1
jacobi petrucciani <j@cobi.dev>
2024-08-05 15:29:46 -0400

    update authentik chart -> v2024-6-3

--- mods/pog/hex/k8s/authentik.nix
+++ mods/pog/hex/k8s/authentik.nix
@@ -8,8 +8,8 @@ let
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v2024-6-2;
-      v2024-6-2 = _v "2024.6.2" "02qrhl5mkjfc59m723klcb28hgvdk975xb8hhkrmh0716y7ijfpc"; # 2024-07-31
+      latest = v2024-6-3;
+      v2024-6-3 = _v "2024.6.3" "1l57srn8qhkhq6yrv2g2cl2ckjnprcg82lr2nz69cxraszw2lr9h"; # 2024-08-05
       v2024-4-2 = _v "2024.4.2" "0in142g0swzn55acll4pcidgqiyxyavbm9gn00dajj8ylcrjz30a"; # 2024-05-07
       v2024-4-1 = _v "2024.4.1" "0890nfigg5x0wds35fzvzmn2l57zad3hv64gaijapwzr8ff63c6i"; # 2024-04-26
       v2024-2-3 = _v "2024.2.3" "046hsz0bn2mf8c12lapkwdznk91l9zq0cjr8wy48nzam88lhzcs9"; # 2024-04-17
