d29ae31e172707b9c071c31f7836b983fe676314
jacobi petrucciani <j@cobi.dev>
2024-10-02 11:00:06 -0400

    update quickwit -> 0.7.1

--- mods/pog/hex/k8s/quickwit.nix
+++ mods/pog/hex/k8s/quickwit.nix
@@ -10,8 +10,8 @@ let
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v0-7-0;
-      v0-7-0 = _v "0.7.0" "150h80cyxlhrxsh0sr5sbjpkvqm1qlxzkfcaym43cqyfjrlxr7x7"; # 2024-08-01
+      latest = v0-7-1;
+      v0-7-1 = _v "0.7.1" "0hcn0z9cpif930cdr47igca5qnfm8iq6mvh83hx4zk98k2lbq2x6"; # 2024-09-18
     };
     chart_url = version: "https://github.com/quickwit-oss/helm-charts/releases/download/${name}-${version}/${name}-${version}.tgz";
     chart = hex.k8s._.chart {
