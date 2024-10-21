b8d444a2bb010b03b31d92bf8a3e2b8d8143cd98
jacobi petrucciani <j@cobi.dev>
2024-10-21 18:49:51 -0400

    run kobold on gpu

--- hosts/styx/configuration.nix
+++ hosts/styx/configuration.nix
@@ -65,6 +65,7 @@ in
           port = 5001;
           model = modelPath "MiniCPM-V-2_6-Q8_0.gguf";
           mmproj = modelPath "mmproj-MiniCPM-V-2_6-f16.gguf";
+          gpulayers = -1;
         };
       };
       llama-server.servers = {
