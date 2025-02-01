c1297c9b0b811e58186c9910f3ce16f7a9cf3046
jacobi petrucciani <j@cobi.dev>
2025-02-01 11:42:58 -0500

    add fix for pypdfium2

--- mods/python/fixes.nix
+++ mods/python/fixes.nix
@@ -75,4 +75,48 @@ rec {
       maintainers = with maintainers; [ joachifm ];
     };
   };
+
+  pypdfium2 = prev.pypdfium2.overridePythonAttrs (_:
+    let
+      headers = final.pkgs.fetchgit {
+        url = "https://pdfium.googlesource.com/pdfium";
+        # The latest revision on the chromium/${pdfiumVersion} branch
+        rev = "f6da7d235728aeaff6586d2190badfb4290a9979";
+        hash = "sha256-Es/IDQPLQGOFhoAY93zduc+eKGpA7qMnLw7TjZpWHvc=";
+      };
+      inherit (final.pkgs) pdfium-binaries;
+      pdfiumVersion = "${pdfium-binaries.version}";
+    in
+    {
+      preBuild =
+        let
+          pdfiumLib = final.pkgs.lib.makeLibraryPath [ pdfium-binaries ];
+          inputVersionFile = (final.pkgs.formats.json { }).generate "version.json" {
+            version = final.pkgs.lib.strings.toInt pdfiumVersion;
+            source = "generated";
+            flags = [ ];
+            run_lds = [ pdfiumLib ];
+            guard_symbols = false;
+          };
+          bindingsDir = "data/bindings";
+          headersDir = "${bindingsDir}/headers";
+          versionFile = "${bindingsDir}/version.json";
+        in
+        ''
+          # Preseed the headers and version file
+          mkdir -p ${bindingsDir}
+          cp -r ${headers}/public ${headersDir}
+          install -m 644 ${inputVersionFile} ${versionFile}
+
+          # Make generated bindings consider pdfium derivation path when loading dynamic libraries
+          substituteInPlace setupsrc/pypdfium2_setup/emplace.py \
+            --replace-fail 'build_pdfium_bindings(pdfium_ver, flags=flags, guard_symbols=True, run_lds=[])' \
+                           'build_pdfium_bindings(pdfium_ver, flags=flags, guard_symbols=True, run_lds=["${pdfiumLib}"])'
+
+          # Short circuit the version pull from the internet
+          substituteInPlace setupsrc/pypdfium2_setup/packaging_base.py \
+            --replace-fail 'PdfiumVer.to_full(build)._asdict()' \
+                           '{"major": 133, "minor": 0, "build": ${pdfiumVersion}, "patch": 1}'
+        '';
+    });
 }
