9df92b1a3a763a91c6190514127910d39be8d7bd
jacobi petrucciani <j@cobi.dev>
2025-03-18 13:19:10 -0400

    python312Packages.nicegui: 2.12.1 -> 2.13.0

--- mods/python/fastapi.nix
+++ mods/python/fastapi.nix
@@ -270,12 +270,12 @@ rec {
 
   nicegui = buildPythonPackage rec {
     pname = "nicegui";
-    version = "2.12.1";
+    version = "2.13.0";
     pyproject = true;
 
     src = fetchPypi {
       inherit pname version;
-      hash = "sha256-n4ELRYlvyzAUJsAWk9F2B91OOwfi63EbvuD4QIs8EhM=";
+      hash = "sha256-orORx4jxXQa67ShqEGUaFZX2lvwqyF4jQ5GxQmir6ks=";
     };
 
     postPatch = ''
@@ -298,6 +298,7 @@ rec {
       "orjson"
       "python-multipart"
       "requests"
+      "starlette"
       "uvicorn"
       "watchfiles"
     ];
