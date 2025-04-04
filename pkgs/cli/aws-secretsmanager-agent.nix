5ed2cea10fdccf9a2200fcc473bb8dc691dfcbc5
jacobi petrucciani <j@cobi.dev>
2025-04-04 09:42:41 -0400

    fix aws-secretsmanager-agent

--- pkgs/cli/aws-secretsmanager-agent.nix
+++ pkgs/cli/aws-secretsmanager-agent.nix
@@ -4,24 +4,26 @@
 , fetchFromGitHub
 , stdenv
 , darwin
+, cacert
 }:
-
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "aws-secretsmanager-agent";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "aws-secretsmanager-agent";
-    rev = "705662d8467a2a1f29bc00501a373a758c771dab"; # actual v1.0.0 tag doesn't seem to build - but this commit is the initial commit of the OSS repo
-    hash = "sha256-8wfA4e+PhDUqd9sFIoWIqL3nEehoK27Z8rD+E28Rqb8=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-/+284hG4wVSaRaRcUJ00BYuVyB8YMgIKGK+DrP6V6rQ=";
   };
 
-  cargoHash = "sha256-FyacZx1s8ApnePkU8q1P0bMXy+WT51yfpI5kM6YIQHA=";
+  cargoHash = "sha256-9yzRGioTICwPEHzUUb8/UGFfKSWOU4Fqa5YZ89Zc/HY=";
 
-  buildInputs = lib.optionals stdenv.isDarwin [
+  buildInputs = [
+    cacert
+  ] ++ (lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
-  ];
+  ]);
 
   meta = with lib; {
     description = "The AWS Secrets Manager Agent is a local HTTP service that you can install and use in your compute environments to read secrets from Secrets Manager and cache them in memory";
