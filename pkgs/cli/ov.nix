7fda39e13b70885f560e1c5928de849dd4dab825
jacobi petrucciani <j@cobi.dev>
2025-04-13 18:10:54 -0400

    ov: 0.32.0 -> 0.40.0
    Diff: https://github.com/noborus/ov/compare/v0.32.0...v0.40.0

--- pkgs/cli/ov.nix
+++ pkgs/cli/ov.nix
@@ -2,13 +2,13 @@
 { lib, buildGo124Module, fetchFromGitHub, installShellFiles }:
 buildGo124Module rec {
   pname = "ov";
-  version = "0.32.0";
+  version = "0.40.0";
 
   src = fetchFromGitHub {
     owner = "noborus";
     repo = "ov";
     rev = "v${version}";
-    sha256 = "sha256-mQ1KwElD8RizOT2trHWo4T1QiZ974xwhQCCa5snpnZM=";
+    sha256 = "sha256-j9zi2QlPecaRiWzDLiVGgxvrxlpQbC+6rfx9DZxR4xw=";
   };
 
   ldflags = [
@@ -17,7 +17,7 @@ buildGo124Module rec {
     "-X main.Version=${version}"
   ];
 
-  vendorHash = "sha256-XACdtJdACMKQ5gSJcjGAPNGPFL1Tbt6QOovl15mvFGI=";
+  vendorHash = "sha256-51jbTtCGOcEyI/FZOKi/vIoaN1i7IZ8Gzk9bcMa344g=";
 
   nativeBuildInputs = [ installShellFiles ];
 
