0989f2576b88fb9ca45c72e4bd7db64b10736dbe
jacobi petrucciani <j@cobi.dev>
2025-04-28 12:35:32 -0400

    mcpo: init at 0.0.12

---
+++ pkgs/uv/README.md
@@ -0,0 +1,11 @@
+# uv
+
+this directory contains apps built with uv2nix using my wrapper
+
+---
+
+## In this directory
+
+### [mcpo.nix](./mcpo.nix)
+
+[mcpo](https://github.com/open-webui/mcpo) is a simple, secure MCP-to-OpenAPI proxy server
---
+++ pkgs/uv/mcpo.nix
@@ -0,0 +1,36 @@
+# [mcpo](https://github.com/open-webui/mcpo) is a simple, secure MCP-to-OpenAPI proxy server
+{ stdenv, lib, fetchFromGitHub, python ? python312, python312, uv-nix }:
+let
+  name = "mcpo";
+  version = "0.0.12";
+
+  src = fetchFromGitHub {
+    owner = "open-webui";
+    repo = "mcpo";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-biLfXQ/4YvbQaL/3YLXFu+31wLwe25JIaCZJ0DMgPCY=";
+  };
+  uvEnv = uv-nix.mkEnv {
+    inherit name;
+    python = python312;
+    workspaceRoot = src;
+    pyprojectOverrides = final: prev: { };
+  };
+in
+stdenv.mkDerivation {
+  inherit version src;
+  pname = name;
+  installPhase = ''
+    runHook preInstall
+    cp -r ${uvEnv} $out
+    runHook postInstall
+  '';
+  meta = {
+    changelog = "https://github.com/open-webui/mcpo/blob/main/CHANGELOG.md";
+    description = "A simple, secure MCP-to-OpenAPI proxy server ";
+    homepage = "https://github.com/open-webui/mcpo";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ jpetrucciani ];
+    mainProgram = "mcpo";
+  };
+}
