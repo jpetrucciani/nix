6f76fc5b87f109ef79fa65ca8f18ea9420e4c2af
jacobi petrucciani <j@cobi.dev>
2025-04-30 12:43:41 -0400

    update styx config

--- hosts/styx/configuration.nix
+++ hosts/styx/configuration.nix
@@ -49,7 +49,7 @@ in
       infinity.enable = true;
       koboldcpp.servers = {
         minicpm = {
-          enable = true;
+          enable = false;
           port = 5001;
           model = modelPath "MiniCPM-V-2_6-Q8_0.gguf";
           mmproj = modelPath "mmproj-MiniCPM-V-2_6-f16.gguf";
@@ -57,27 +57,13 @@ in
         };
       };
       llama-server.servers = {
-        gemma3 = {
+        qwen3-30b-a3 = {
           enable = true;
           package = pkgs.llama-cpp-latest;
           port = 8012;
-          model = modelPath "gemma-3-27b-it-Q5_K_M.gguf";
-          extraFlags = ''--ctx-size 16384 --seed 3407 --prio 2 --temp 1.0 --repeat-penalty 1.0 --min-p 0.01 --top-k 64 --top-p 0.95'';
-          ngl = 99;
-        };
-        r1-1-5b = {
-          enable = true;
-          package = pkgs.llama-cpp-latest;
-          port = 8013;
-          model = modelPath "DeepSeek-R1-Distill-Qwen-1.5B-Q8_0.gguf";
-          ngl = 99;
-        };
-        deepscaler-1-5b = {
-          enable = true;
-          package = pkgs.llama-cpp-latest;
-          port = 8014;
-          model = modelPath "agentica-org_DeepScaleR-1.5B-Preview-Q8_0.gguf";
-          ngl = 99;
+          model = modelPath "Qwen3-30B-A3B-UD-Q4_K_XL.gguf";
+          extraFlags = ''--ctx-size 32768 --seed 420 --prio 2 --temp 0.6 --min-p 0.0 --top-k 20 --top-p 0.95'';
+          ngl = 999;
         };
       };
       mlx-vlm-api.servers.qwen-2-5-vl-7b.enable = true;
