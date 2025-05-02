2e005391fa20d40c7b73bf8e5d5b8c9573709f49
jacobi petrucciani <j@cobi.dev>
2025-05-02 09:29:05 -0400

    fix formatting

--- pkgs/uv/vllm.nix
+++ pkgs/uv/vllm.nix
@@ -1,5 +1,5 @@
 # [vllm](https://github.com/vllm-project/vllm) is a high-throughput and memory-efficient inference and serving engine for LLMs
-{ lib, stdenv, uv-nix }:
+{ lib, uv-nix }:
 uv-nix.buildUvPackage rec {
   pname = "vllm";
   version = "0.8.5";
