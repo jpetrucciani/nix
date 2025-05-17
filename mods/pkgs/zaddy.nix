321ee3ab13413e37bb64764539809b1811c7557c
jacobi petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2025-05-16 21:57:15 -0400

    automatic update (#1937)
    * automatic update
    
    * update zaddy hash

--- mods/pkgs/zaddy.nix
+++ mods/pkgs/zaddy.nix
@@ -115,7 +115,7 @@ rec {
       googleclouddns
       route53
     ];
-    vendorHash = "sha256-7k5KobsresXiTLL5d9W6Q9yJ9Pi+cTIR1eqsfxL5MoQ=";
+    vendorHash = "sha256-EUJqeo6sWT5N334MQ73iNVcOMVEalu31vescu9ckm5g=";
   };
 
   # caddy with s3-browser plugin
