4a90c78a56002aeda2c8f2db6c4f3e739ea6f6db
jacobi petrucciani <j@cobi.dev>
2025-05-21 00:14:13 +0000

    automatic update (supabase-cli-stable)

--- pkgs/cli/supabase-cli-stable.nix
+++ pkgs/cli/supabase-cli-stable.nix
@@ -9,16 +9,16 @@
 
 buildGoModule rec {
   pname = "supabase-cli-stable";
-  version = "2.22.12";
+  version = "2.23.4";
 
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "cli";
     rev = "v${version}";
-    hash = "sha256-3Sbk71wMl7gnWb1G3O2HpLBvLNoux6UyY/AWgcs9Hl4=";
+    hash = "sha256-WtOyvc58uYB7B5iK9VdujaRUBcKyVcQRhgjTlXEklWg=";
   };
 
-  vendorHash = "sha256-ONADn0MAxUyZYtdIbWhnIXaItALOGwM6QoxQyovaw9M=";
+  vendorHash = "sha256-ttQNief5BHbsRtmA32VFMbF2cuiIrtXVfOw5C/A9R+Q=";
 
   ldflags = [
     "-s"
