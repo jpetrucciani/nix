fc3a8671de7903148d63099e04905d55eb13bb9f
jacobi petrucciani <j@cobi.dev>
2025-07-21 17:55:24 -0400

    pogocache: init at 1.0.0

---
+++ pkgs/server/pogocache.nix
@@ -0,0 +1,54 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, git
+, openssl
+, liburing
+, perl
+}:
+let
+  onlyDarwin = value: if stdenv.isDarwin then value else null;
+in
+stdenv.mkDerivation rec {
+  pname = "pogocache";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "tidwall";
+    repo = "pogocache";
+    rev = "v${version}";
+    hash = "sha256-8J+NzFrml4dHgYAVsLQoqwbX/sRp686chBEjK/Z3Vow=";
+  };
+
+  ${onlyDarwin "NOURING"} = "1";
+  ${onlyDarwin "NOOPENSSL"} = "1";
+
+  buildInputs = [ git ] ++ (lib.optionals stdenv.isLinux [ perl liburing openssl ]);
+
+  postPatch = lib.optionals stdenv.isLinux ''
+    sed -i -e '/download.sh/,+7d' ./deps/build-openssl.sh
+    tar -xzf ${openssl.src}
+    mv openssl-* deps/openssl
+
+    substituteInPlace ./deps/build-uring.sh \
+      --replace-fail './download.sh https://github.com/axboe/liburing liburing $vers liburing-$vers' ""
+    cp -r ${liburing.src} ./deps/liburing
+
+    patchShebangs ./deps/*.sh ./deps/openssl/Configure
+    chmod -R +w ./deps
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./pogocache $out/bin/.
+  '';
+
+  meta = {
+    description = "Fast caching software with a focus on low latency and cpu efficiency";
+    homepage = "https://github.com/tidwall/pogocache";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ jpetrucciani ];
+    mainProgram = "pogocache";
+    platforms = lib.platforms.all;
+  };
+}
