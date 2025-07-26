50be7117fb86fd2a81dd661c08b483aa1ca590f8
jacobi petrucciani <j@cobi.dev>
2025-07-26 07:33:26 -0400

    ty: 0.0.1-alpha.15 -> 0.0.1-alpha.16
    Diff: https://github.com/astral-sh/ty/compare/refs/tags/0.0.1-alpha.15...refs/tags/0.0.1-alpha.16

--- mods/python/experimental.nix
+++ mods/python/experimental.nix
@@ -6,14 +6,14 @@ in
 rec {
   ty = buildPythonPackage rec {
     pname = "ty";
-    version = "0.0.1-alpha.15";
+    version = "0.0.1-alpha.16";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "astral-sh";
       repo = "ty";
       rev = "refs/tags/${version}";
-      hash = "sha256-7aOFRrnj4WFTuQWcOgdSU3w9KWwRbpPj3K6g4y0JPo0=";
+      hash = "sha256-hpDzl1TJRCfr5l76HwK90WAbAgeDR48eRNs9knj87lk=";
       fetchSubmodules = true;
     };
 
@@ -26,7 +26,7 @@ rec {
       lockFile = "${src}/ruff/Cargo.lock";
       outputHashes = {
         "lsp-types-0.95.1" = "sha256-8Oh299exWXVi6A39pALOISNfp8XBya8z+KT/Z7suRxQ=";
-        "salsa-0.23.0" = "sha256-jVL/Y548jv8RJ1rgBdJcsGtwIsH2aLxBbgO4FZl78wc=";
+        "salsa-0.23.0" = "sha256-ov667FHJ6fStoleoR/EovtPORJFF7ogJe00mx9cI7rk=";
       };
     };
 
