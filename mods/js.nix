d3bd36dc8b79ebcee233143a89be75c8be10565e
jacobi petrucciani <j@cobi.dev>
2025-11-20 17:36:15 -0500

    please be the last one?

--- mods/js.nix
+++ mods/js.nix
@@ -1,4 +1,4 @@
 # this overlay injects various javascript helpers into scope
 final: prev: {
-  pnpm2nix = (import ../flake-compat.nix).inputs.pnpm2nix.packages.${prev.system};
+  pnpm2nix = (import ../flake-compat.nix).inputs.pnpm2nix.packages.${prev.stdenv.hostPlatform.system};
 }
