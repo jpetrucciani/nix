c6dc938198fea36566b0af5a68b6b4bb38c96c3e
jacobi petrucciani <j@cobi.dev>
2026-01-28 06:47:18 -0500

    t: 0.0.4 -> 0.0.5
    Diff: https://github.com/alecthomas/t/compare/v0.0.4...v0.0.5

--- pkgs/cli/t.nix
+++ pkgs/cli/t.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "t";
-  version = "0.0.4";
+  version = "0.0.5";
 
   src = fetchFromGitHub {
     owner = "alecthomas";
     repo = "t";
     rev = "v${version}";
-    hash = "sha256-qbNJhyupjkMim9L5U9J3SWeA1olM1Cd+fwldzyGyHZc=";
+    hash = "sha256-ZNZc8B0F2z0C5WRoq2YD/dciJNl64ScOA3p4yNOwe9A=";
   };
 
   cargoHash = "sha256-Vpisck1TMU3iubfA77DSBExEnwtVV/eibcC+qMR0+Y8=";
@@ -20,6 +20,7 @@ rustPlatform.buildRustPackage rec {
   meta = {
     description = "T is a concise language for manipulating text, replacing common usage patterns of Unix utilities like grep, sed, cut, awk, sort, and uniq";
     homepage = "https://github.com/alecthomas/t";
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ jpetrucciani ];
     mainProgram = "t";
   };
