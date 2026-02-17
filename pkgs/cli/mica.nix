e3d2cc6f92393a8424a42276371ab55ea8e99a40
jacobi petrucciani <j@cobi.dev>
2026-02-16 23:51:10 -0500

    mica: 0.0.3 -> 0.0.4

--- pkgs/cli/mica.nix
+++ pkgs/cli/mica.nix
@@ -3,29 +3,37 @@
 , fetchFromGitHub
 , pkg-config
 , sqlite
-
+, installShellFiles
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "mica";
-  version = "0.0.3";
+  version = "0.0.4";
 
   src = fetchFromGitHub {
     owner = "gemologic";
     repo = "mica";
     rev = "v${version}";
-    hash = "sha256-CXxwv3CYIOlcYJzMCsqMpQCPEE0MbOTSRNa4oBCMrVw=";
+    hash = "sha256-4Aleo54km+rLdry1TWRvCqQVThDz2JaVqwrhsZCwKmc=";
   };
 
-  cargoHash = "sha256-kJK+akh9/IlJ2zhuz3SvPAh3ajEGmzjHMhN9JWSi3go=";
+  cargoHash = "sha256-MS53DKsqR9C+fqujjz7N/QuQtqtKDavGzad+CPjtTZ8=";
 
   nativeBuildInputs = [
     pkg-config
+    installShellFiles
   ];
 
   buildInputs = [
     sqlite
   ];
+
+  postInstall = ''
+    installShellCompletion --cmd mica \
+      --bash <($out/bin/mica completion bash) \
+      --fish <($out/bin/mica completion fish) \
+      --zsh <($out/bin/mica completion zsh)
+  '';
 
   meta = {
     description = "An experimental TUI for managing nix environments";
