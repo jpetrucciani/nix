bd0d82840e8418328b2edd31d9ca4b23937bb828
jacobi petrucciani <j@cobi.dev>
2026-02-20 08:58:14 -0500

    weathr: init at 1.3.0

---
+++ pkgs/cli/weathr.nix
@@ -0,0 +1,34 @@
+{ lib
+, stdenv
+, rustPlatform
+, fetchFromGitHub
+, installShellFiles
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "weathr";
+  version = "1.3.0";
+
+  src = fetchFromGitHub {
+    owner = "Veirt";
+    repo = "weathr";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-JwI5a+O5Nu39Nr0st5yBLTM5kPLC8UIGAoBMqxnOOl4=";
+  };
+
+  cargoHash = "sha256-Yj1WxpOLL8GiVpCebPZQgdw+L9g+4CNY7n2z8PJQz4k=";
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  doCheck = false; # tests require network
+
+  meta = {
+    description = "A terminal weather app with ascii animation";
+    homepage = "https://github.com/Veirt/weathr";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ jpetrucciani ];
+    mainProgram = "weathr";
+  };
+})
