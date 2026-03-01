85f1d750c36063ef901de8018cc999351bce9323
jacobi petrucciani <j@cobi.dev>
2026-03-01 01:09:14 -0500

    update caddy-security

--- mods/pkgs/zaddy.nix
+++ mods/pkgs/zaddy.nix
@@ -6,7 +6,7 @@ rec {
       builtins = {
         caddy-cgi = { name = "github.com/aksdb/caddy-cgi"; version = "7cf2523251ffeef310868d8ed03e17a929236f2e"; };
         caddy-exec = { name = "github.com/abiosoft/caddy-exec"; version = "06d4f7218eb886ab9664e63c3f56010992e93fb9"; };
-        caddy-security = { name = "github.com/greenpau/caddy-security"; version = "v1.1.32"; };
+        caddy-security = { name = "github.com/greenpau/caddy-security"; version = "v1.1.33"; };
         caddy-webhook = { name = "github.com/WingLim/caddy-webhook"; version = "v1.0.8"; };
         certmagic-storage-dynamodb = { name = "github.com/silinternational/certmagic-storage-dynamodb"; version = "3.0.0"; };
 
@@ -78,7 +78,7 @@ rec {
         allPlugins;
       zaddy = prev.caddy.override {
         buildGo125Module = args:
-          let _args = args { inherit version; }; in final.buildGo125Module (_args // {
+          let _args = args { inherit version; }; in final.buildGo126Module (_args // {
             inherit src vendorHash;
             overrideModAttrs = _: {
               preBuild = ''
@@ -112,7 +112,7 @@ rec {
       googleclouddns
       route53
     ];
-    vendorHash = "sha256-LJVcXGmruS5jHWWpX/CCz7J/IXTd+sHGdOsTZmEolQY=";
+    vendorHash = "sha256-gW7WshirfDN7j53rXdX5UwiqaSLk4+yEt8tSM2aJFQ4=";
   };
 
   # caddy with s3-browser plugin
