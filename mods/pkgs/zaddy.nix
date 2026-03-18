f76fbae2255dc79b2249f02bb65b96bbb603bf8b
jacobi petrucciani <j@cobi.dev>
2026-03-18 18:43:14 -0400

    update caddy-security

--- mods/pkgs/zaddy.nix
+++ mods/pkgs/zaddy.nix
@@ -6,7 +6,7 @@ rec {
       builtins = {
         caddy-cgi = { name = "github.com/aksdb/caddy-cgi"; version = "7cf2523251ffeef310868d8ed03e17a929236f2e"; };
         caddy-exec = { name = "github.com/abiosoft/caddy-exec"; version = "06d4f7218eb886ab9664e63c3f56010992e93fb9"; };
-        caddy-security = { name = "github.com/greenpau/caddy-security"; version = "v1.1.49"; };
+        caddy-security = { name = "github.com/greenpau/caddy-security"; version = "v1.1.50"; };
         caddy-webhook = { name = "github.com/WingLim/caddy-webhook"; version = "v1.0.8"; };
         certmagic-storage-dynamodb = { name = "github.com/silinternational/certmagic-storage-dynamodb"; version = "3.0.0"; };
 
@@ -112,7 +112,7 @@ rec {
       googleclouddns
       route53
     ];
-    vendorHash = "sha256-ce7jCLm9o0kxyvNJLVv/K7pWwpjBLnRk9euKnkKI0z8=";
+    vendorHash = "sha256-tXN95XuHLwlGrTuiOMBWjdFXbzaDfCUQl7GSzz3CpBg=";
   };
 
   # caddy with s3-browser plugin
