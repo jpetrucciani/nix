bc2fd19a79c9070a211f3a62f042f519cd7e5846
jacobi petrucciani <j@cobi.dev>
2026-04-20 15:46:47 -0400

    codex-latest: 0.121.0 -> 0.122.0

--- mods/final.nix
+++ mods/final.nix
@@ -167,13 +167,13 @@ let
 
   codex-latest =
     let
-      version = "0.121.0";
+      version = "0.122.0";
       v8Version = "146.4.0";
       src = final.fetchFromGitHub {
         owner = "openai";
         repo = "codex";
         tag = "rust-v${version}";
-        hash = "sha256-wjiUMox9V5tFggNgaFyHXWhRlpPerK7W+U/eR2Ddbbc=";
+        hash = "sha256-CpXWP64URsgt/PhQrUkrT87KG633hxRUIY0wWrTFmjk=";
       };
       # `rusty_v8` tries to fetch this archive during the build, which fails in
       # Nix's sandbox. Pre-fetch it instead and point the build script at it.
@@ -218,7 +218,7 @@ let
       cargoDeps = final.rustPlatform.fetchCargoVendor {
         inherit src;
         sourceRoot = "${src.name}/codex-rs";
-        hash = "sha256-zpQ0vg9XuarLfdZYiRIhcwLHUOdunNbOb5xLW3MPzp8=";
+        hash = "sha256-2qtMLWSdYWJ+blNfCHXtgmzizuM1HgpTGa5RQ3U/AEM=";
       };
       env =
         (old.env or { })
