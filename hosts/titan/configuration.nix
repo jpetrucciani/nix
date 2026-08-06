5f531369dd20055a5f76c00c1b008e768556777a
jacobi petrucciani <j@cobi.dev>
2026-08-05 23:18:51 -0400

    more memory tweaks?

--- hosts/titan/configuration.nix
+++ hosts/titan/configuration.nix
@@ -18,7 +18,7 @@ let
     stt = {
       modelPath = "Qwen/Qwen3-ASR-1.7B";
       port = 8012;
-      memFractionStatic = 0.42;
+      memFractionStatic = 0.5;
       extraEnv = { CUDA_VISIBLE_DEVICES = "1"; };
     };
   };
