5abe9df825cf2eeb18448e6235e5621eafb0ea37
jacobi petrucciani <j@cobi.dev>
2026-08-11 10:00:12 -0400

    vllm: 0.27.0 -> 0.27.1

--- pkgs/uv/vllm.nix
+++ pkgs/uv/vllm.nix
@@ -5,8 +5,8 @@
 , cudatoolkit
 , clang
 , ninja
-, version ? "0.27.0"
-, lockHash ? "sha256-lOvl4p3BR4N+vCFaD1KrotgJqciO/pfPYfwrXncynGQ="
+, version ? "0.27.1"
+, lockHash ? "sha256-zBHxxOJYqXVzSZ2qOsmcBHTimu3Qxqulbz1S1ZdvMCY="
 , isWSL ? false
 , includePin ? false
 }:
